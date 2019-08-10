cb4221fd50247138eaa626e468c09a56955786c7
Haris <harissahovic@gmail.com>
2019-08-10 02:48:01 +0200

    Add constant file with typeChart path

---
+++ pokemon_showdown_env/constants.py
@@ -0,0 +1,13 @@
+"""Constants file
+
+This file contains constant values used in the repository.
+
+Attributes:
+    TYPE_CHART_PATH (str): Path to the json file containing type informations.
+"""
+
+import os
+
+
+TYPE_CHART_PATH = os.path.join("data", "typeChart.json")
+"str: Path to the json file containing type informations."
