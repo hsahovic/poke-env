73c347b81557bf7bce1e3d527c21967549c8e6c4
Haris <harissahovic@gmail.com>
2019-08-10 03:20:29 +0200

    Add tests for TYPE_CHART and compute_type_chart

---
+++ test/test_utils.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from pokemon_showdown_env.utils import compute_type_chart, TYPE_CHART
+from pokemon_showdown_env.environment.type import Type
+
+
+def test_type_chart_corresponds_to_compute_type_chart():
+    assert compute_type_chart() == TYPE_CHART
+
+
+def test_types_correspond_to_type_chart_entries():
+    for type_1 in list(Type):
+        assert type_1.name in TYPE_CHART
+        for type_2 in list(Type):
+            assert type_2.name in TYPE_CHART
+
+    for type_ in TYPE_CHART.keys():
+        assert Type[type_] is not None
+
+
+def test_some_type_weaknesses():
+    assert TYPE_CHART["WATER"]["WATER"] == 0.5
+    assert TYPE_CHART["FIRE"]["DRAGON"] == 0.5
+    assert TYPE_CHART["ELECTRIC"]["DRAGON"] == 0.5
+
+
+def test_some_type_strengths():
+    assert TYPE_CHART["WATER"]["FIRE"] == 2
+    assert TYPE_CHART["DRAGON"]["DRAGON"] == 2
+    assert TYPE_CHART["ELECTRIC"]["WATER"] == 2
+
+
+def test_some_type_immunities():
+    assert TYPE_CHART["GROUND"]["FLYING"] == 0
+    assert TYPE_CHART["DRAGON"]["FAIRY"] == 0
+    assert TYPE_CHART["ELECTRIC"]["GROUND"] == 0
