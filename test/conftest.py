c32ae22aac3ae589757d7883c42affac672928a5
Haris <harissahovic@gmail.com>
2019-08-10 03:19:15 +0200

    Add conftest with pre-test addition of the module's root to pythonpath

---
+++ test/conftest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+def pytest_configure(config):
+    """Adds the module's root directory to PYTHONPATH for tests.
+    """
+    import os
+    import sys
+
+    sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/.."))
