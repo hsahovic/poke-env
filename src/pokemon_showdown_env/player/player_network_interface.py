65f633874807ac6bf87cc5fedb06ea9bfdcf6a42
hsahovic <harissahovic@gmail.com>
2019-09-13 17:58:45 -0400

    Basic websocket listening loop in PlayerNetwork

--- src/pokemon_showdown_env/player/player_network_interface.py
+++ src/pokemon_showdown_env/player/player_network_interface.py
@@ -2,6 +2,8 @@
 """This module defines a base class communicating with showdown servers.
 """
 
+import websockets
+
 from abc import ABC
 
 
@@ -14,6 +16,16 @@ class PlayerNetwork(ABC):
 
     def __init__(self, *, server_url: str) -> None:
         self._server_url = server_url
+
+    async def handle_message(self, message: str) -> None:
+        pass
+
+    async def listen(self) -> None:
+        """Listen to a showdown websocket."""
+        async with websockets.connect(self.websocket_url) as websocket:
+            self._websocket = websocket
+            message = await websocket.recv()
+            await self.handle_message(message)
 
     @property
     def websocket_url(self) -> str:
