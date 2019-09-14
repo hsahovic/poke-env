4e3acee4348c654546cffbc5c43ad29e97e42cb7
hsahovic <harissahovic@gmail.com>
2019-09-14 02:56:40 -0400

    Extend init parameters for player network interface

--- src/pokemon_showdown_env/player/player_network_interface.py
+++ src/pokemon_showdown_env/player/player_network_interface.py
@@ -5,6 +5,8 @@
 import websockets
 
 from abc import ABC
+from asyncio import Lock
+from typing import Optional
 
 
 class PlayerNetwork(ABC):
@@ -14,7 +16,32 @@ class PlayerNetwork(ABC):
     Responsible for communicating with showdown servers.
     """
 
-    def __init__(self, *, server_url: str) -> None:
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        *,
+        avatar: Optional[int] = None,
+        authentication_url: str,
+        server_url: str,
+    ) -> None:
+        """
+        :param username: Player username.
+        :type username: str
+        :param password: Player password.
+        :type password: str
+        :param avatar: Player avatar id. Optional.
+        :type avatar: int, optional
+        :param authentication_url: Authentication server url.
+        :type authentication_url: str
+        :param server_url: Server URL.
+        :type server_url: str
+        """
+        self._authentication_url = authentication_url
+        self._avatar = avatar
+        self._lock = Lock()
+        self._password = password
+        self._username = username
         self._server_url = server_url
 
     async def handle_message(self, message: str) -> None:
