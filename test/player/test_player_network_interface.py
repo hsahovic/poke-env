ff67dcbe241fc4b6bf59e967ae999344acf213da
hsahovic <harissahovic@gmail.com>
2019-09-18 14:32:04 -0400

    Add tests skeleton for player network

---
+++ test/player/test_player_network_interface.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from pokemon_showdown_env.player.player_network_interface import PlayerNetwork
+
+
+class PlayerNetworkChild(PlayerNetwork):
+    def handle_battle_message(self, message):
+        pass
+
+    def update_challenges(self):
+        pass
+
+
+def test_init_and_properties():
+    pass
+
+
+def test_create_player_logger():
+    pass
+
+
+def test_log_in():
+    pass
+
+
+def test_change_avatar():
+    pass
+
+
+def test_handle_message():
+    pass
+
+
+def test_listen():
+    pass
+
+
+def test_send_message():
+    pass
