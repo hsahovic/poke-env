c4ba5a03ecf2b6807f2791bfd3a559f830a5757d
hsahovic <harissahovic@gmail.com>
2019-09-22 17:40:41 -0400

    Add tests for change avatar and listen, rewrite test for log in in PNI

--- test/player/test_player_network_interface.py
+++ test/player/test_player_network_interface.py
@@ -2,20 +2,14 @@
 
 import asyncio
 import logging
+import pytest
+import websockets
 
 from collections import namedtuple
-from mock import MagicMock
-from mock import patch
+from asynctest.mock import CoroutineMock
+from asynctest.mock import patch
+from asynctest.mock import PropertyMock
 from pokemon_showdown_env.player.player_network_interface import PlayerNetwork
-
-
-# This hack comes from
-# https://stackoverflow.com/questions/51394411/python-object-magicmock-cant-be-used-in-await-expression
-async def async_magic():
-    pass
-
-
-MagicMock.__await__ = lambda x: async_magic().__await__()
 
 
 requests_tuple = namedtuple("requests_tuple", ["text"])
@@ -56,11 +50,12 @@ def test_create_player_logger():
     assert logger.getEffectiveLevel() == 24
 
 
+@pytest.mark.asyncio
 @patch(
     "pokemon_showdown_env.player.player_network_interface.requests.post",
     return_value=requests_tuple(':{"assertion":"content"}'),
 )
-def test_log_in(post_mock):
+async def test_log_in(post_mock):
     player = PlayerNetworkChild(
         "username",
         "password",
@@ -70,26 +65,69 @@ def test_log_in(post_mock):
         log_level=38,
     )
 
-    player.send_message = MagicMock()
-    player.change_avatar = MagicMock()
+    player.send_message = CoroutineMock()
+    player.change_avatar = CoroutineMock()
 
-    asyncio.run(player._log_in(["A", "B", "C", "D"]))
+    await player._log_in(["A", "B", "C", "D"])
 
     player.change_avatar.assert_called_once_with(12)
     player.send_message.assert_called_once_with("/trn username,0,content")
     post_mock.assert_called_once()
 
 
-def test_change_avatar():
+@pytest.mark.asyncio
+async def test_change_avatar():
+    player = PlayerNetworkChild(
+        "username",
+        "password",
+        avatar=12,
+        authentication_url="auth.url",
+        server_url="server.url",
+    )
+
+    player.send_message = CoroutineMock()
+
+    await player.change_avatar(8)
+
+    player.send_message.assert_called_once_with("/avatar 8")
+
+
+@pytest.mark.asyncio
+async def test_handle_message():
     pass
 
 
-def test_handle_message():
-    pass
+@pytest.mark.asyncio
+async def test_listen():
+    player = PlayerNetworkChild(
+        "username",
+        "password",
+        avatar=12,
+        authentication_url="auth.url",
+        server_url="server.url",
+    )
 
+    type(player).websocket_url = PropertyMock(return_value="ws://localhost:8899")
 
-def test_listen():
-    pass
+    player.handle_message = CoroutineMock()
+    semaphore = asyncio.Semaphore()
+
+    async def showdown_server_mock(websocket, path):
+        semaphore.release()
+        await websocket.ping()
+        await websocket.send("test 1")
+        await websocket.send("test 2")
+        await websocket.send("test 3")
+
+    await semaphore.acquire()
+
+    gathered = asyncio.gather(websockets.serve(showdown_server_mock, "0.0.0.0", 8899))
+
+    await player.listen()
+
+    await gathered
+    assert player.handle_message.await_count == 3
+    player.handle_message.assert_awaited_with("test 3")
 
 
 def test_send_message():
