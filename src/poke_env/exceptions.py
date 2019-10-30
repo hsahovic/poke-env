9f036bb70d42b6609a11dfd9fafd2bc846728c58
hsahovic <harissahovic@gmail.com>
2019-10-30 15:47:56 -0400

    Initial commit for first release of exceptions

---
+++ src/poke_env/exceptions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""This module contains exceptions.
+"""
+
+
+class ShowdownException(Exception):
+    """This exception is raised when a non-managed message is received from the server.
+    """
+
+    pass
