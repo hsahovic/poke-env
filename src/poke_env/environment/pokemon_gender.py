436a217909fe801302e5889d01771901286133e3
hsahovic <harissahovic@gmail.com>
2019-10-30 15:46:50 -0400

    Initial commit for first release of pokemon gender

---
+++ src/poke_env/environment/pokemon_gender.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from enum import Enum, unique, auto
+from poke_env.exceptions import ShowdownException
+
+
+@unique
+class PokemonGender(Enum):
+    """Represent a pokemon's gender."""
+
+    FEMALE = auto()
+    MALE = auto()
+    NEUTRAL = auto()
+
+    def __str__(self) -> str:
+        return f"{self.name} (pokemon gender) object"
+
+    @staticmethod
+    def from_request_details(gender: str) -> "PokemonGender":
+        """Returns the PokemonGenre object corresponding to the gender received in a message.
+
+        :param gender: The received gender to convert.
+        :type gender: str
+        :return: The corresponding PokemonGenre object.
+        :rtype: PokemonGenre
+        """
+        if gender == "M":
+            return PokemonGender.MALE
+        elif gender == "F":
+            return PokemonGender.FEMALE
+        raise ShowdownException("Unmanaged request gender: '%s'", gender)
