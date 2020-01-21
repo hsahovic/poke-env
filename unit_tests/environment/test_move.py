51c96ef5eef1c2837d3587947ecdac17d694368c
hsahovic <harissahovic@gmail.com>
2020-01-21 14:47:16 -0500

    Reordering of tests for move

--- unit_tests/environment/test_move.py
+++ unit_tests/environment/test_move.py
@@ -10,9 +10,62 @@ def move_generator():
         yield Move(move)
 
 
+def test_accuracy():
+    volt_thunderbolt = Move("10000000voltthunderbolt")
+    absorb = Move("absorb")
+    aeroblast = Move("aeroblast")
+
+    assert volt_thunderbolt.accuracy == 1
+    assert absorb.accuracy == 1
+    assert aeroblast.accuracy == 0.95
+
+    for move in move_generator():
+        assert isinstance(move.accuracy, float)
+        assert 0 <= move.accuracy <= 1
+
+
 def test_all_moves_instanciate():
     for move in move_generator():
         pass
+
+
+def test_drain():
+    draining_kiss = Move("drainingkiss")
+    flame_thrower = Move("flamethrower")
+
+    assert draining_kiss.drain == 0.75
+    assert flame_thrower.drain == 0
+
+    for move in move_generator():
+        assert isinstance(move.drain, float)
+        assert 0 <= move.drain <= 1
+
+
+def test_heal():
+    roost = Move("roost")
+    flame_thrower = Move("flamethrower")
+
+    assert roost.heal == 0.5
+    assert flame_thrower.heal == 0
+
+    for move in move_generator():
+        assert isinstance(move.heal, float)
+        assert 0 <= move.heal <= 1
+
+
+def test_ignore_immunity():
+    thousand_arrows = Move("thousandarrows")
+    thunder_wave = Move("thunderwave")
+    bide = Move("bide")
+    flame_thrower = Move("flamethrower")
+
+    assert thousand_arrows.ignore_immunity == {PokemonType["GROUND"]}
+    assert thunder_wave.ignore_immunity is False
+    assert bide.ignore_immunity is True
+    assert flame_thrower.ignore_immunity is False
+
+    for move in move_generator():
+        assert type(move.ignore_immunity) in [bool, set]
 
 
 def test_move_base_power():
@@ -24,6 +77,31 @@ def test_move_base_power():
 def test_move_breaks_protect():
     for move in move_generator():
         assert isinstance(move.breaks_protect, bool)
+
+
+def test_n_hit():
+    furys_wipes = Move("furyswipes")
+    flame_thrower = Move("flamethrower")
+    gear_grind = Move("geargrind")
+
+    assert furys_wipes.n_hit == (2, 5)
+    assert flame_thrower.n_hit == (1, 1)
+    assert gear_grind.n_hit == (2, 2)
+
+    for move in move_generator():
+        assert isinstance(move.n_hit, tuple) and len(move.n_hit) == 2
+
+
+def test_recoil():
+    flare_blitz = Move("flareblitz")
+    flame_thrower = Move("flamethrower")
+
+    assert flare_blitz.recoil == 0.33
+    assert flame_thrower.recoil == 0
+
+    for move in move_generator():
+        assert isinstance(move.recoil, float)
+        assert 0 <= move.recoil <= 1
 
 
 def test_self_boosts():
@@ -52,81 +130,3 @@ def test_status():
 
     for move in move_generator():
         assert move.status is None or isinstance(move.status, Status)
-
-
-def test_drain():
-    draining_kiss = Move("drainingkiss")
-    flame_thrower = Move("flamethrower")
-
-    assert draining_kiss.drain == 0.75
-    assert flame_thrower.drain == 0
-
-    for move in move_generator():
-        assert isinstance(move.drain, float)
-        assert 0 <= move.drain <= 1
-
-
-def test_heal():
-    roost = Move("roost")
-    flame_thrower = Move("flamethrower")
-
-    assert roost.heal == 0.5
-    assert flame_thrower.heal == 0
-
-    for move in move_generator():
-        assert isinstance(move.heal, float)
-        assert 0 <= move.heal <= 1
-
-
-def test_recoil():
-    flare_blitz = Move("flareblitz")
-    flame_thrower = Move("flamethrower")
-
-    assert flare_blitz.recoil == 0.33
-    assert flame_thrower.recoil == 0
-
-    for move in move_generator():
-        assert isinstance(move.recoil, float)
-        assert 0 <= move.recoil <= 1
-
-
-def test_n_hit():
-    furys_wipes = Move("furyswipes")
-    flame_thrower = Move("flamethrower")
-    gear_grind = Move("geargrind")
-
-    assert furys_wipes.n_hit == (2, 5)
-    assert flame_thrower.n_hit == (1, 1)
-    assert gear_grind.n_hit == (2, 2)
-
-    for move in move_generator():
-        assert isinstance(move.n_hit, tuple) and len(move.n_hit) == 2
-
-
-def test_accuracy():
-    volt_thunderbolt = Move("10000000voltthunderbolt")
-    absorb = Move("absorb")
-    aeroblast = Move("aeroblast")
-
-    assert volt_thunderbolt.accuracy == 1
-    assert absorb.accuracy == 1
-    assert aeroblast.accuracy == 0.95
-
-    for move in move_generator():
-        assert isinstance(move.accuracy, float)
-        assert 0 <= move.accuracy <= 1
-
-
-def test_ignore_immunity():
-    thousand_arrows = Move("thousandarrows")
-    thunder_wave = Move("thunderwave")
-    bide = Move("bide")
-    flame_thrower = Move("flamethrower")
-
-    assert thousand_arrows.ignore_immunity == {PokemonType["GROUND"]}
-    assert thunder_wave.ignore_immunity is False
-    assert bide.ignore_immunity is True
-    assert flame_thrower.ignore_immunity is False
-
-    for move in move_generator():
-        assert type(move.ignore_immunity) in [bool, set]
