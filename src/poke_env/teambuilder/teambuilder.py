b60e139a3e64d3da809186e1934a7529c6c1d7fa
hsahovic <harissahovic@gmail.com>
2020-04-02 19:33:31 -0400

    Add team conversion utility to Teambuilder

--- src/poke_env/teambuilder/teambuilder.py
+++ src/poke_env/teambuilder/teambuilder.py
@@ -3,9 +3,22 @@
 Pokemon Showdown teams in the context of communicating with Pokemon Showdown.
 """
 from abc import ABC, abstractmethod
+from poke_env.teambuilder.teambuilder_pokemon import TeambuilderPokemon
+from typing import List
 
 
 class Teambuilder(ABC):
     @abstractmethod
     def yield_team(self):
         pass
+
+    @staticmethod
+    def join_team(team: List[TeambuilderPokemon]) -> str:
+        """Converts a list of TeambuilderPokemon objects into the corresponding packed
+        showdown team format.
+
+        :param team: The list of TeambuilderPokemon objects that form the team.
+        :type team: list
+        :return: The formatted team string.
+        :type: str"""
+        return "]".join([mon.formatted for mon in team])
