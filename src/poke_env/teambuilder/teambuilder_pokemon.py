3df2b86c24a771ffb3aee904302e5cf357abff4e
hsahovic <harissahovic@gmail.com>
2020-04-02 19:28:56 -0400

    Add formatting utilities to TeambuilderPokemon

--- src/poke_env/teambuilder/teambuilder_pokemon.py
+++ src/poke_env/teambuilder/teambuilder_pokemon.py
@@ -2,6 +2,7 @@
 """This module defines the TeambuilderPokemon class, which is used as an intermediate
 format to specify pokemon builds in teambuilders custom classes.
 """
+from poke_env.utils import to_id_str
 
 
 class TeambuilderPokemon:
@@ -48,3 +49,45 @@ class TeambuilderPokemon:
             self.moves = []
         else:
             self.moves = moves
+
+    @property
+    def formatted_evs(self):
+        f_evs = ",".join([str(el) if el != 0 else "" for el in self.evs])
+        if f_evs == "," * 5:
+            return ""
+        return f_evs
+
+    @property
+    def formatted_ivs(self):
+        f_ivs = ",".join([str(el) if el != 31 else "" for el in self.ivs])
+        if f_ivs == "," * 5:
+            return ""
+        return f_ivs
+
+    @property
+    def formatted_moves(self):
+        return ",".join([to_id_str(move) for move in self.moves])
+
+    @property
+    def formatted_endstring(self):
+        if self.hiddenpowertype:
+            return ",%s," % self.hiddenpowertype
+        return ""
+
+    @property
+    def formatted(self):
+        return "%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s%s" % (
+            self.nickname or "",
+            to_id_str(self.species) if self.species else "",
+            to_id_str(self.item) if self.item else "",
+            to_id_str(self.ability) if self.ability else "",
+            self.formatted_moves or "",
+            self.nature or "",
+            self.formatted_evs or "",
+            self.gender or "",
+            self.formatted_ivs or "",
+            "S" if self.shiny else "",
+            self.level or "",
+            self.happiness or "",
+            self.formatted_endstring,
+        )
