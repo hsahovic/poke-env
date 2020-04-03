283f43a3cfd78eaa0961118bee0989a243d8f63e
hsahovic <harissahovic@gmail.com>
2020-04-02 20:31:19 -0400

    Add teambuilder module to project init

--- src/poke_env/__init__.py
+++ src/poke_env/__init__.py
@@ -7,6 +7,7 @@ import poke_env.exceptions as exceptions
 import poke_env.player as player
 import poke_env.player_configuration as player_configuration
 import poke_env.server_configuration as server_configuration
+import poke_env.teambuilder as teambuilder
 import poke_env.utils as utils
 
 __all__ = [
@@ -16,5 +17,6 @@ __all__ = [
     "player",
     "player_configuration",
     "server_configuration",
+    "teambuilder",
     "utils",
 ]
