66b1b3e99c311b68f7dd3fdec817125082b34e56
hsahovic <harissahovic@gmail.com>
2020-04-14 18:27:24 -0400

    Add custom exception raising in effect

--- src/poke_env/environment/effect.py
+++ src/poke_env/environment/effect.py
@@ -2,6 +2,9 @@
 """This module defines the Effect class, which represents in-game effects.
 """
 # pyre-ignore-all-errors[45]
+
+from poke_env.exceptions import UnexpectedEffectException
+
 from enum import Enum, unique, auto
 
 
@@ -84,4 +87,7 @@ class Effect(Enum):
         message = message.replace("move: ", "")
         message = message.replace("ability: ", "")
         message = message.replace(" ", "_")
-        return Effect[message.upper()]
+        try:
+            return Effect[message.upper()]
+        except KeyError:
+            raise UnexpectedEffectException("Unexpected effect '%s' received" % message)
