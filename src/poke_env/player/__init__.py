fb083e4ca9cfb89ac442b1b0ab6343c36cf2fa17
hsahovic <harissahovic@gmail.com>
2020-04-29 01:34:19 -0400

    Add env player to player init

--- src/poke_env/player/__init__.py
+++ src/poke_env/player/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """poke_env.player module init.
 """
+from poke_env.player import env_player
 from poke_env.player import player
 from poke_env.player import player_network_interface
 from poke_env.player import random_player
@@ -8,6 +9,7 @@ from poke_env.player import trainable_player
 from poke_env.player import utils
 
 __all__ = [
+    "env_player",
     "player",
     "player_network_interface",
     "random_player",
