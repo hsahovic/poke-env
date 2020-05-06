65bc820a018e080256c4e18ac06ae096f5b772e5
hsahovic <harissahovic@gmail.com>
2020-05-06 19:10:39 -0400

    Update dynamax tests

--- unit_tests/environment/test_dynamax.py
+++ unit_tests/environment/test_dynamax.py
@@ -14,10 +14,6 @@ async def test_dynamax():
     battle._player_role = "p1"
 
     hydreigon = Pokemon(species="hydreigon")
-    max_hp_before_dynamax = 326
-    current_hp_before_dynamax = 100
-    hydreigon._max_hp = max_hp_before_dynamax
-    hydreigon._current_hp = current_hp_before_dynamax
     hydreigon._active = True
     battle._team = {"p1: Hydreigon": hydreigon}
     assert battle.active_pokemon.is_dynamaxed is not True
@@ -25,14 +21,10 @@ async def test_dynamax():
     await battle._parse_message(["", "-start", "p1: Hydreigon", "dynamax"])
 
     assert battle.active_pokemon.is_dynamaxed
-    assert battle.active_pokemon._max_hp == max_hp_before_dynamax * 2
-    assert battle.active_pokemon._current_hp == current_hp_before_dynamax * 2
     assert battle.dynamax_turns_left == 3
 
     await battle._parse_message(["", "-end", "p1: Hydreigon", "dynamax"])
 
     assert not battle.active_pokemon.is_dynamaxed
-    assert battle.active_pokemon._max_hp == max_hp_before_dynamax
-    assert battle.active_pokemon._current_hp == current_hp_before_dynamax
     assert battle.dynamax_turns_left is None
     assert not battle.can_dynamax
