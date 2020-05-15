71da47d7b4488865d936b5d38c557d2c4d4535d9
hsahovic <harissahovic@gmail.com>
2020-05-15 18:57:21 -0400

    Add integration test for laddering

---
+++ integration_tests/test_laddering.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import asyncio
+import pytest
+
+from poke_env.player.random_player import RandomPlayer
+
+
+async def simple_ladderring(n_battles, max_concurrent_battles):
+    players = [
+        RandomPlayer(max_concurrent_battles=max_concurrent_battles),
+        RandomPlayer(max_concurrent_battles=max_concurrent_battles),
+    ]
+
+    await asyncio.gather(players[0].ladder(n_battles), players[1].ladder(n_battles))
+
+    for player in players:
+        await player.stop_listening()
+
+
+@pytest.mark.asyncio
+async def test_laddering():
+    for n_battles in [1, 3, 5, 10]:
+        await asyncio.wait_for(simple_ladderring(n_battles, 1), timeout=n_battles)
+        await asyncio.wait_for(
+            simple_ladderring(n_battles, n_battles), timeout=n_battles
+        )
