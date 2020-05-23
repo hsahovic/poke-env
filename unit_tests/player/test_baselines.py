9d6e0954eca68442dedc08e47f2aeb2f9e174658
hsahovic <harissahovic@gmail.com>
2020-05-23 17:29:27 -0400

    Add unit test for SimpleHeuristicPlayer._estimate_matchup

--- unit_tests/player/test_baselines.py
+++ unit_tests/player/test_baselines.py
@@ -2,6 +2,7 @@
 from poke_env.environment.move import Move
 from poke_env.environment.pokemon import Pokemon
 from poke_env.player.baselines import MaxBasePowerPlayer
+from poke_env.player.baselines import SimpleHeuristicsPlayer
 from collections import namedtuple
 
 
@@ -33,3 +34,29 @@ def test_max_base_power_player():
 
     battle.available_moves.append(Move("flamethrower"))
     assert player.choose_move(battle) == "/choose move flamethrower"
+
+
+def test_simple_heuristics_player_estimate_matchup():
+    player = SimpleHeuristicsPlayer(start_listening=False)
+
+    dragapult = Pokemon(species="dragapult")
+    assert player._estimate_matchup(dragapult, dragapult) == 0
+
+    gengar = Pokemon(species="gengar")
+    assert player._estimate_matchup(dragapult, gengar) == -player._estimate_matchup(
+        gengar, dragapult
+    )
+    assert player._estimate_matchup(dragapult, gengar) == player.SPEED_TIER_COEFICIENT
+
+    mamoswine = Pokemon(species="mamoswine")
+    assert (
+        player._estimate_matchup(dragapult, mamoswine)
+        == -1 + player.SPEED_TIER_COEFICIENT
+    )
+
+    dragapult._set_hp("100/100")
+    mamoswine._set_hp("50/100")
+    assert (
+        player._estimate_matchup(dragapult, mamoswine)
+        == -1 + player.SPEED_TIER_COEFICIENT + player.HP_FRACTION_COEFICIENT / 2
+    )
