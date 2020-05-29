6390861ffe3f21ee9db3839c40a423d807582b95
hsahovic <harissahovic@gmail.com>
2020-05-29 05:20:21 -0400

    Add battle unit_test for weather and field interactions

--- unit_tests/environment/test_battle.py
+++ unit_tests/environment/test_battle.py
@@ -4,8 +4,10 @@ import pytest
 from unittest.mock import MagicMock
 
 from poke_env.environment.battle import Battle
+from poke_env.environment.field import Field
 from poke_env.environment.pokemon_type import PokemonType
 from poke_env.environment.side_condition import SideCondition
+from poke_env.environment.weather import Weather
 
 
 def test_battle_get_pokemon():
@@ -60,9 +62,58 @@ def test_battle_side_start_end():
 
     condition = "safeguard"
     battle._side_start("p1", condition)
-
+    battle._side_start("p2", condition)
     assert battle.side_conditions == {SideCondition.SAFEGUARD}
+    assert battle.opponent_side_conditions == {SideCondition.SAFEGUARD}
 
     battle._side_end("p1", condition)
-
+    battle._side_end("p2", condition)
     assert not battle.side_conditions
+    assert not battle.opponent_side_conditions
+
+    with pytest.raises(Exception):
+        battle._side_end("p1", condition)
+
+    with pytest.raises(Exception):
+        battle._side_end("p2", condition)
+
+
+def test_battle_field_interactions():
+    logger = MagicMock()
+    battle = Battle("tag", "username", logger)
+
+    assert not battle.fields
+
+    battle._parse_message(["", "-fieldstart", "Electric terrain"])
+    assert battle.fields == {Field.ELECTRIC_TERRAIN}
+
+    battle._parse_message(["", "-fieldstart", "Trick room"])
+    assert battle.fields == {Field.ELECTRIC_TERRAIN, Field.TRICK_ROOM}
+
+    battle._parse_message(["", "-fieldend", "Trick room"])
+    assert battle.fields == {Field.ELECTRIC_TERRAIN}
+
+    battle._parse_message(["", "-fieldend", "Electric terrain"])
+    assert not battle.fields
+
+    with pytest.raises(Exception):
+        battle._parse_message(["", "-fieldend", "Electric terrain"])
+
+    with pytest.raises(Exception):
+        battle._parse_message(["", "-fieldend", "non existent field"])
+
+
+def test_battle_weather_interactions():
+    logger = MagicMock()
+    battle = Battle("tag", "username", logger)
+
+    assert battle.weather is None
+
+    battle._parse_message(["", "-weather", "desolateland"])
+    assert battle.weather == Weather.DESOLATELAND
+
+    battle._parse_message(["", "-weather", "hail"])
+    assert battle.weather == Weather.HAIL
+
+    battle._parse_message(["", "-weather", "none"])
+    assert battle.weather is None
