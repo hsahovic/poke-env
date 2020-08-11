3359c6b82e06d7c0c47bddbf11159515a8de07ca
AFumis <58889866+AFumis@users.noreply.github.com>
2020-08-10 22:19:30 -0300

    fix bug in expected_hits

--- src/poke_env/environment/move.py
+++ src/poke_env/environment/move.py
@@ -197,7 +197,7 @@ class Move:
         if self._id=="triplekick" or self._id=="tripleaxel":
             #Triple Kick and Triple Axel have an accuracy check for each hit, and also rise in BP for each hit
             return 1+2*0.9+3*0.81
-        min, max = self.n_hits
+        min, max = self.n_hit
         if min==max:
             return min
         else:
