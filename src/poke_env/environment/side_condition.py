bb2128ad2cccc2a41eed0fbe1c0e7d4b7650f326
hsahovic <harissahovic@gmail.com>
2020-10-27 23:49:46 +0100

    Add pledges SideConditions

--- src/poke_env/environment/side_condition.py
+++ src/poke_env/environment/side_condition.py
@@ -11,9 +11,11 @@ class SideCondition(Enum):
     """Enumeration, represent a in-battle side condition."""
 
     AURORA_VEIL = auto()
+    FIRE_PLEDGE = auto()
     G_MAX_CANNONADE = auto()
     G_MAX_STEELSURGE = auto()
     G_MAX_VOLCALITH = auto()
+    GRASS_PLEDGE = auto()
     LIGHT_SCREEN = auto()
     LUCKY_CHANT = auto()
     MIST = auto()
@@ -24,6 +26,7 @@ class SideCondition(Enum):
     STICKY_WEB = auto()
     TAILWIND = auto()
     TOXIC_SPIKES = auto()
+    WATER_PLEDGE = auto()
 
     def __str__(self) -> str:
         return f"{self.name} (side condition) object"
