1f7318224cc60dc4e1ef6565f742e1258fa5e439
hsahovic <harissahovic@gmail.com>
2020-11-03 02:54:39 +0100

    Update battle parsing tests

--- unit_tests/environment/test_battle.py
+++ unit_tests/environment/test_battle.py
@@ -302,7 +302,7 @@ def test_battle_request_and_interactions(example_request):
     battle._parse_message(["", "switch", "p1: Latias", "Latias, L82", "100/100"])
     assert battle.opponent_active_pokemon.base_stats["def"] == 90
     battle._parse_message(["", "-mega", "p1: Latias", "latiasite"])
-    assert battle.opponent_active_pokemon.species == "Latias-Mega"
+    assert battle.opponent_active_pokemon.species == "Latias"
     assert battle.opponent_active_pokemon.base_stats["def"] == 120
 
     battle._parse_message(["", "-mustrecharge", "p1: Latias"])
@@ -315,7 +315,7 @@ def test_battle_request_and_interactions(example_request):
 
     battle._parse_message(["", "switch", "p1: Groudon", "Groudon, L82", "100/100"])
     battle._parse_message(["", "-primal", "p1: Groudon"])
-    assert battle.opponent_active_pokemon.species == "Groudon-Primal"
+    assert battle.opponent_active_pokemon.species == "Groudon"
 
     battle._parse_message(["", "-setboost", "p1: Groudon", "atk", "6"])
     assert battle.opponent_active_pokemon.boosts["atk"] == 6
