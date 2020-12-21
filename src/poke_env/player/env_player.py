28d1eca9997fcf3d3c0fe351d4f0044aec73a38b
hsahovic <harissahovic@gmail.com>
2020-12-22 00:55:24 +0100

    Add gen 4, 5 and 6 single player en player classes

--- src/poke_env/player/env_player.py
+++ src/poke_env/player/env_player.py
@@ -361,6 +361,122 @@ class EnvPlayer(Player, Env, ABC):  # pyre-ignore
         pass
 
 
+class Gen4EnvSinglePlayer(EnvPlayer):  # pyre-ignore
+    _ACTION_SPACE = list(range(4 + 6))
+    _DEFAULT_BATTLE_FORMAT = "gen4randombattle"
+
+    def _action_to_move(  # pyre-ignore
+        self, action: int, battle: Battle
+    ) -> BattleOrder:
+        """Converts actions to move orders.
+
+        The conversion is done as follows:
+
+        0 <= action < 4:
+            The actionth available move in battle.available_moves is executed.
+        4 <= action < 10
+            The action - 4th available switch in battle.available_switches is executed.
+
+        If the proposed action is illegal, a random legal move is performed.
+
+        :param action: The action to convert.
+        :type action: int
+        :param battle: The battle in which to act.
+        :type battle: Battle
+        :return: the order to send to the server.
+        :rtype: str
+        """
+        if (
+            action < 4
+            and action < len(battle.available_moves)
+            and not battle.force_switch
+        ):
+            return self.create_order(battle.available_moves[action])
+        elif 0 <= action - 4 < len(battle.available_switches):
+            return self.create_order(battle.available_switches[action - 4])
+        else:
+            return self.choose_random_move(battle)
+
+    @property
+    def action_space(self) -> List:
+        """The action space for gen 7 single battles.
+
+        The conversion to moves is done as follows:
+
+        0 <= action < 4:
+            The actionth available move in battle.available_moves is executed.
+        4 <= action < 10
+            The action - 4th available switch in battle.available_switches is executed.
+        """
+        return self._ACTION_SPACE
+
+
+class Gen5EnvSinglePlayer(Gen4EnvSinglePlayer):  # pyre-ignore
+    _DEFAULT_BATTLE_FORMAT = "gen5randombattle"
+
+
+class Gen6EnvSinglePlayer(EnvPlayer):  # pyre-ignore
+    _ACTION_SPACE = list(range(2 * 4 + 6))
+    _DEFAULT_BATTLE_FORMAT = "gen6randombattle"
+
+    def _action_to_move(  # pyre-ignore
+        self, action: int, battle: Battle
+    ) -> BattleOrder:
+        """Converts actions to move orders.
+
+        The conversion is done as follows:
+
+        0 <= action < 4:
+            The actionth available move in battle.available_moves is executed.
+        4 <= action < 8:
+            The action - 8th available move in battle.available_moves is executed, with
+            mega-evolution.
+        8 <= action < 14
+            The action - 8th available switch in battle.available_switches is executed.
+
+        If the proposed action is illegal, a random legal move is performed.
+
+        :param action: The action to convert.
+        :type action: int
+        :param battle: The battle in which to act.
+        :type battle: Battle
+        :return: the order to send to the server.
+        :rtype: str
+        """
+        if (
+            action < 4
+            and action < len(battle.available_moves)
+            and not battle.force_switch
+        ):
+            return self.create_order(battle.available_moves[action])
+        elif (
+            battle.can_mega_evolve
+            and 0 <= action - 4 < len(battle.available_moves)
+            and not battle.force_switch
+        ):
+            return self.create_order(battle.available_moves[action - 8], mega=True)
+        elif 0 <= action - 12 < len(battle.available_switches):
+            return self.create_order(battle.available_switches[action - 12])
+        else:
+            return self.choose_random_move(battle)
+
+    @property
+    def action_space(self) -> List:
+        """The action space for gen 7 single battles.
+
+        The conversion to moves is done as follows:
+
+        0 <= action < 4:
+            The actionth available move in battle.available_moves is executed.
+        4 <= action < 8:
+            The action - 8th available move in battle.available_moves is executed, with
+            mega-evolution.
+        8 <= action < 14
+            The action - 8th available switch in battle.available_switches is executed.
+        """
+        return self._ACTION_SPACE
+
+
 class Gen7EnvSinglePlayer(EnvPlayer):  # pyre-ignore
     _ACTION_SPACE = list(range(3 * 4 + 6))
     _DEFAULT_BATTLE_FORMAT = "gen7randombattle"
