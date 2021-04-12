8f0b0649287e3b9b960b1a94a8a603ec466ba409
hsahovic <harissahovic@gmail.com>
2021-04-13 00:41:12 +0200

    Add available_moves_from_request method to Pokemon objects

--- src/poke_env/environment/pokemon.py
+++ src/poke_env/environment/pokemon.py
@@ -10,7 +10,7 @@ from poke_env.data import POKEDEX
 from poke_env.environment.effect import Effect
 from poke_env.environment.pokemon_gender import PokemonGender
 from poke_env.environment.pokemon_type import PokemonType
-from poke_env.environment.move import Move
+from poke_env.environment.move import Move, SPECIAL_MOVES
 from poke_env.environment.status import Status
 from poke_env.environment.z_crystal import Z_CRYSTAL
 from poke_env.utils import to_id_str
@@ -401,6 +401,28 @@ class Pokemon:
         self._max_hp = None
         self._status = None
         self._switch_out()
+
+    def available_moves_from_request(self, request: Dict) -> List[Move]:
+        moves = []
+
+        request_moves = [
+            move["id"] for move in request["moves"] if not move.get("disabled", False)
+        ]
+        for move in request_moves:
+            if move in self.moves:
+                moves.append(self.moves[move])
+            elif move in SPECIAL_MOVES:
+                moves.append(SPECIAL_MOVES[move])
+            else:
+                assert {
+                    "copycat",
+                    "metronome",
+                    "mefirst",
+                    "mirrormove",
+                    "assist",
+                }.intersection(self.moves)
+                moves.append(Move(move))
+        return moves
 
     def damage_multiplier(self, type_or_move: Union[PokemonType, Move]) -> float:
         """
