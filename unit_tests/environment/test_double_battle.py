22828cd72727225ade23c9b4b498d73ffed50c6e
hsahovic <harissahovic@gmail.com>
2021-05-09 02:01:08 +0200

    Add misc battle tests

--- unit_tests/environment/test_double_battle.py
+++ unit_tests/environment/test_double_battle.py
@@ -35,6 +35,14 @@ def test_battle_request_parsing_and_interactions(example_doubles_request):
 
     battle._parse_request(example_doubles_request)
     mr_rime, klinklang = battle.active_pokemon
+    (
+        my_first_active,
+        my_second_active,
+        their_first_active,
+        their_second_active,
+    ) = battle.all_active_pokemons
+    assert my_first_active == mr_rime and my_second_active == klinklang
+    assert their_first_active is None and their_second_active is None
     assert isinstance(mr_rime, Pokemon)
     assert isinstance(klinklang, Pokemon)
     assert battle.get_pokemon("p1: Mr. Rime") == mr_rime
