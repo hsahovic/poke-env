973ee156cbadd231e53f7c934864b5951fbf70c3
hsahovic <harissahovic@gmail.com>
2021-07-10 04:55:20 +0200

    Split integration tests in smaller tests

--- integration_tests/test_concurrency_control.py
+++ integration_tests/test_concurrency_control.py
@@ -35,28 +35,40 @@ async def simple_cross_evaluation(n_battles, n_concurrent_battles):
     return players[-1].move_history
 
 
+async def check_max_concurrent_battle_works(n_battles, n_concurrent_battles):
+    result = await asyncio.wait_for(
+        simple_cross_evaluation(n_battles, n_concurrent_battles), timeout=n_battles + 30
+    )
+
+    times_per_battle = {}
+    for i, val in enumerate(result):
+        if val not in times_per_battle:
+            times_per_battle[val] = []
+        times_per_battle[val].append(i)
+
+    start_end = [(values[0], values[-1]) for values in times_per_battle.values()]
+    max_concurrent_battles = 0
+
+    for i, val in enumerate(result):
+        current_battles = 0
+        for start, end in start_end:
+            if start <= i <= end:
+                current_battles += 1
+        max_concurrent_battles = max(max_concurrent_battles, current_battles)
+
+    assert max_concurrent_battles == n_concurrent_battles
+
+
 @pytest.mark.asyncio
-async def test_max_concurrent_battle_works():
-    for max_battles in [1, 3, 5]:
-        result = await asyncio.wait_for(
-            simple_cross_evaluation(7 * max_battles, max_battles),
-            timeout=5 * max_battles + 15,
-        )
+async def test_max_concurrent_battle_works_no_concurrency():
+    await check_max_concurrent_battle_works(7, 1)
 
-        times_per_battle = {}
-        for i, val in enumerate(result):
-            if val not in times_per_battle:
-                times_per_battle[val] = []
-            times_per_battle[val].append(i)
 
-        start_end = [(values[0], values[-1]) for values in times_per_battle.values()]
-        max_concurrent_battles = 0
+@pytest.mark.asyncio
+async def test_max_concurrent_battle_works_3_concurrent():
+    await check_max_concurrent_battle_works(20, 3)
 
-        for i, val in enumerate(result):
-            current_battles = 0
-            for start, end in start_end:
-                if start <= i <= end:
-                    current_battles += 1
-            max_concurrent_battles = max(max_concurrent_battles, current_battles)
 
-        assert max_concurrent_battles == max_battles
+@pytest.mark.asyncio
+async def test_max_concurrent_battle_works_5_concurrent():
+    await check_max_concurrent_battle_works(30, 5)
