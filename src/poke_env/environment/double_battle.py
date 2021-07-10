2e93bdd21a635a063e504b5737c0c119ef64287e
hsahovic <harissahovic@gmail.com>
2021-07-10 04:56:34 +0200

    Fix minor bug in maxTeamSize request parsing

--- src/poke_env/environment/double_battle.py
+++ src/poke_env/environment/double_battle.py
@@ -109,7 +109,8 @@ class DoubleBattle(AbstractBattle):
 
         if request.get("teamPreview", False):
             self._teampreview = True
-            self._max_team_size = request["maxTeamSize"]
+            number_of_mons = len(request["side"]["pokemon"])
+            self._max_team_size = request.get("maxTeamSize", number_of_mons)
         else:
             self._teampreview = False
 
