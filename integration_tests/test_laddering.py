dc9da0d388294eb2eb105a3f606dec7ca8adbff6
Haris Sahovic <25432355+hsahovic@users.noreply.github.com>
2021-07-15 00:23:20 +0200

    Increase timeout limit in integration_tests/test_laddering.test_laddering

--- integration_tests/test_laddering.py
+++ integration_tests/test_laddering.py
@@ -20,7 +20,7 @@ async def simple_ladderring(n_battles, max_concurrent_battles):
 @pytest.mark.asyncio
 async def test_laddering():
     for n_battles in [1, 5]:
-        await asyncio.wait_for(simple_ladderring(n_battles, 1), timeout=n_battles)
+        await asyncio.wait_for(simple_ladderring(n_battles, 1), timeout=n_battles + 5)
         await asyncio.wait_for(
             simple_ladderring(n_battles, n_battles), timeout=n_battles
         )
