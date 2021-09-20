c4ed9177c29015c1389bc330f9afc21e3002f61d
hsahovic <harissahovic@gmail.com>
2021-09-20 03:21:45 +0200

    Up version to 0.4.19

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ URL = "https://github.com/hsahovic/poke-env"
 EMAIL = "contact@sahovic.fr"
 AUTHOR = "Haris Sahovic"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.4.18"
+VERSION = "0.4.19"
 
 # What packages are required for this module to be executed?
 with open("requirements.txt") as requirements:
