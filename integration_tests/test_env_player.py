edf477544382a53f099b1b4d151b1fe064f4ae50
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-02-10 00:49:14 +0100

    Conform tests to new APIs

--- integration_tests/test_env_player.py
+++ integration_tests/test_env_player.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from typing import Union
+
 import numpy as np
 import pytest
+from gym import Space
 
 from poke_env.player.env_player import (
     Gen4EnvSinglePlayer,
@@ -9,39 +12,80 @@ from poke_env.player.env_player import (
     Gen7EnvSinglePlayer,
     Gen8EnvSinglePlayer,
 )
+from poke_env.player.openai_api import EnvLoop
 from poke_env.player.random_player import RandomPlayer
 
 
 class RandomGen4EnvPlayer(Gen4EnvSinglePlayer):
-    MAX_BATTLE_SWITCH_RETRY = 500
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def calc_reward(self, last_battle, current_battle) -> float:
+        return 0.0
+
+    def describe_embedding(self) -> Space:
+        return None
 
     def embed_battle(self, battle):
         return np.array([0])
 
 
 class RandomGen5EnvPlayer(Gen5EnvSinglePlayer):
-    MAX_BATTLE_SWITCH_RETRY = 500
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def calc_reward(self, last_battle, current_battle) -> float:
+        return 0.0
+
+    def describe_embedding(self) -> Space:
+        return None
 
     def embed_battle(self, battle):
         return np.array([0])
 
 
 class RandomGen6EnvPlayer(Gen6EnvSinglePlayer):
-    MAX_BATTLE_SWITCH_RETRY = 500
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def calc_reward(self, last_battle, current_battle) -> float:
+        return 0.0
+
+    def describe_embedding(self) -> Space:
+        return None
 
     def embed_battle(self, battle):
         return np.array([0])
 
 
 class RandomGen7EnvPlayer(Gen7EnvSinglePlayer):
-    MAX_BATTLE_SWITCH_RETRY = 500
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def calc_reward(self, last_battle, current_battle) -> float:
+        return 0.0
+
+    def describe_embedding(self) -> Space:
+        return None
 
     def embed_battle(self, battle):
         return np.array([0])
 
 
 class RandomGen8EnvPlayer(Gen8EnvSinglePlayer):
-    MAX_BATTLE_SWITCH_RETRY = 500
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def calc_reward(self, last_battle, current_battle) -> float:
+        return 0.0
+
+    def describe_embedding(self) -> Space:
+        return None
 
     def embed_battle(self, battle):
         return np.array([0])
@@ -52,75 +96,60 @@ def play_function(player, n_battles):
         done = False
         player.reset()
         while not done:
-            _, _, done, _ = player.step(np.random.choice(player.action_space))
+            _, _, done, _ = player.step(np.random.choice(player.action_space.sample()))
 
 
 @pytest.mark.timeout(30)
 def test_random_gym_players_gen4():
-    env_player = RandomGen4EnvPlayer(log_level=20)
-    random_player = RandomPlayer(battle_format="gen4randombattle", log_level=20)
-    env_player.play_against(
-        env_algorithm=play_function,
-        opponent=random_player,
-        env_algorithm_kwargs={"n_battles": 3},
-    )
+    with EnvLoop():
+        random_player = RandomPlayer(battle_format="gen4randombattle", log_level=20)
+        env_player = RandomGen4EnvPlayer(log_level=20, opponent=random_player, start_challenging=False)
+        env_player.start_challenging(3)
+        play_function(env_player, 3)
 
 
 @pytest.mark.timeout(30)
 def test_random_gym_players_gen5():
-    env_player = RandomGen5EnvPlayer(log_level=20)
-    random_player = RandomPlayer(battle_format="gen5randombattle", log_level=20)
-    env_player.play_against(
-        env_algorithm=play_function,
-        opponent=random_player,
-        env_algorithm_kwargs={"n_battles": 3},
-    )
+    with EnvLoop():
+        random_player = RandomPlayer(battle_format="gen5randombattle", log_level=20)
+        env_player = RandomGen5EnvPlayer(log_level=20, opponent=random_player, start_challenging=False)
+        env_player.start_challenging(3)
+        play_function(env_player, 3)
 
 
 @pytest.mark.timeout(30)
 def test_random_gym_players_gen6():
-    env_player = RandomGen6EnvPlayer(log_level=20)
-    random_player = RandomPlayer(battle_format="gen6randombattle", log_level=20)
-    env_player.play_against(
-        env_algorithm=play_function,
-        opponent=random_player,
-        env_algorithm_kwargs={"n_battles": 3},
-    )
+    with EnvLoop():
+        random_player = RandomPlayer(battle_format="gen5randombattle", log_level=20)
+        env_player = RandomGen5EnvPlayer(log_level=20, opponent=random_player, start_challenging=False)
+        env_player.start_challenging(3)
+        play_function(env_player, 3)
 
 
 @pytest.mark.timeout(30)
 def test_random_gym_players_gen7():
-    env_player = RandomGen7EnvPlayer(log_level=20)
-    random_player = RandomPlayer(battle_format="gen7randombattle", log_level=20)
-    env_player.play_against(
-        env_algorithm=play_function,
-        opponent=random_player,
-        env_algorithm_kwargs={"n_battles": 3},
-    )
+    with EnvLoop():
+        random_player = RandomPlayer(battle_format="gen6randombattle", log_level=20)
+        env_player = RandomGen6EnvPlayer(log_level=20, opponent=random_player, start_challenging=False)
+        env_player.start_challenging(3)
+        play_function(env_player, 3)
 
 
 @pytest.mark.timeout(30)
 def test_random_gym_players_gen8():
-    env_player = RandomGen8EnvPlayer(log_level=20)
-    random_player = RandomPlayer(battle_format="gen8randombattle", log_level=20)
-    env_player.play_against(
-        env_algorithm=play_function,
-        opponent=random_player,
-        env_algorithm_kwargs={"n_battles": 3},
-    )
+    with EnvLoop():
+        random_player = RandomPlayer(battle_format="gen8randombattle", log_level=20)
+        env_player = RandomGen8EnvPlayer(log_level=20, opponent=random_player, start_challenging=False)
+        env_player.start_challenging(3)
+        play_function(env_player, 3)
 
 
 @pytest.mark.timeout(60)
 def test_two_successive_calls_gen8():
-    env_player = RandomGen8EnvPlayer(battle_format="gen8randombattle", log_level=20)
-    random_player = RandomPlayer(battle_format="gen8randombattle", log_level=20)
-    env_player.play_against(
-        env_algorithm=play_function,
-        opponent=random_player,
-        env_algorithm_kwargs={"n_battles": 2},
-    )
-    env_player.play_against(
-        env_algorithm=play_function,
-        opponent=random_player,
-        env_algorithm_kwargs={"n_battles": 2},
-    )
+    with EnvLoop():
+        random_player = RandomPlayer(battle_format="gen4randombattle", log_level=20)
+        env_player = RandomGen4EnvPlayer(log_level=20, opponent=random_player, start_challenging=False)
+        env_player.start_challenging(2)
+        play_function(env_player, 2)
+        env_player.start_challenging(2)
+        play_function(env_player, 2)
