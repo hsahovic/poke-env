98d3df23a2bb437d899e718d4986ff18d134be9a
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-02-10 03:32:58 +0100

    Fix laddering test while keeping (I hope) the rationale

--- integration_tests/test_laddering.py
+++ integration_tests/test_laddering.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import asyncio
 import pytest
+import time
 
 from poke_env.player.random_player import RandomPlayer
 
 
-async def simple_ladderring(n_battles, max_concurrent_battles):
+async def simple_laddering(n_battles, max_concurrent_battles):
     players = [
         RandomPlayer(max_concurrent_battles=max_concurrent_battles, log_level=20),
         RandomPlayer(max_concurrent_battles=max_concurrent_battles, log_level=20),
@@ -19,8 +20,14 @@ async def simple_ladderring(n_battles, max_concurrent_battles):
 
 @pytest.mark.asyncio
 async def test_laddering():
-    for n_battles in [1, 5]:
-        await asyncio.wait_for(simple_ladderring(n_battles, 1), timeout=n_battles + 5)
-        await asyncio.wait_for(
-            simple_ladderring(n_battles, n_battles), timeout=n_battles
-        )
+    for n_battles in [2, 6]:
+        start_sequential_battle = time.time()
+        await asyncio.wait_for(simple_laddering(n_battles, 1), None)
+        end_sequential_battle = time.time()
+        sequential_duration = end_sequential_battle - start_sequential_battle
+        start_parallel_battle = time.time()
+        await asyncio.wait_for(simple_laddering(n_battles, n_battles), None)
+        end_parallel_battle = time.time()
+        parallel_duration = end_parallel_battle - start_parallel_battle
+
+        assert parallel_duration < sequential_duration
