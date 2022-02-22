50336e759aaa5a6edf98036172b48ddb68576f05
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-02-22 03:45:59 +0100

    Add example for new RL API

--- src/poke_env/player/utils.py
+++ src/poke_env/player/utils.py
@@ -25,7 +25,7 @@ _EVALUATION_RATINGS = {
 
 def background_cross_evaluate(
     players: List[Player], n_challenges: int
-) -> Future[Dict[str, Dict[str, Optional[float]]]]:
+) -> "Future[Dict[str, Dict[str, Optional[float]]]]":  # pragma: no cover
     return asyncio.run_coroutine_threadsafe(
         cross_evaluate(players, n_challenges), POKE_LOOP
     )
@@ -104,7 +104,7 @@ def _estimate_strength_from_results(
 
 def background_evaluate_player(
     player, n_battles: int = 1000, n_placement_battles: int = 30
-) -> Future[Tuple[float, Tuple[float, float]]]:
+) -> "Future[Tuple[float, Tuple[float, float]]]":  # pragma: no cover
     return asyncio.run_coroutine_threadsafe(
         evaluate_player(player, n_battles, n_placement_battles), POKE_LOOP
     )
