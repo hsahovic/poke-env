89bca096958805d0123425c0b5a21e6104f0e9bf
Cameron Angliss <cameronangliss99@gmail.com>
2023-08-05 22:05:33 -0400

    fix unit test

--- unit_tests/environment/test_pokemon.py
+++ unit_tests/environment/test_pokemon.py
@@ -4,10 +4,10 @@ from poke_env.environment import Move, Pokemon, PokemonType
 def test_pokemon_moves():
     mon = Pokemon(species="charizard", gen=8)
 
-    mon._moved("flamethrower")
+    mon.moved("flamethrower")
     assert "flamethrower" in mon.moves
 
-    mon._moved("struggle")
+    mon.moved("struggle")
     assert "struggle" not in mon.moves
 
     assert not mon.preparing
@@ -35,12 +35,12 @@ def test_pokemon_types():
     assert mon.type_1 == PokemonType.DRAGON
     assert mon.type_2 == PokemonType.FLYING
 
-    mon._mega_evolve("altariaite")
+    mon.mega_evolve("altariaite")
     assert mon.type_1 == PokemonType.DRAGON
     assert mon.type_2 == PokemonType.FAIRY
 
     mon = Pokemon(species="charizard", gen=8)
-    mon._mega_evolve("charizarditex")
+    mon.mega_evolve("charizarditex")
     assert mon.type_1 == PokemonType.FIRE
     assert mon.type_2 == PokemonType.DRAGON
 
@@ -49,7 +49,7 @@ def test_pokemon_types():
     assert mon.type_1 == PokemonType.GROUND
     assert mon.type_2 is None
 
-    mon._primal()
+    mon.primal()
     assert mon.type_1 == PokemonType.GROUND
     assert mon.type_2 == PokemonType.FIRE
 
@@ -75,27 +75,27 @@ def test_powerherb_ends_move_preparation():
     mon = Pokemon(species="roserade", gen=8)
     mon.item = "powerherb"
 
-    mon._prepare("solarbeam", "talonflame")
+    mon.prepare("solarbeam", "talonflame")
     assert mon.preparing
 
-    mon._end_item("powerherb")
+    mon.end_item("powerherb")
     assert not mon.preparing
 
 
 def test_protect_counter_interactions():
     mon = Pokemon(species="xerneas", gen=8)
-    mon._moved("protect", failed=False)
+    mon.moved("protect", failed=False)
 
     assert mon.protect_counter == 1
 
-    mon._start_effect("feint")
+    mon.start_effect("feint")
     assert mon.protect_counter == 0
 
-    mon._moved("protect", failed=False)
-    mon._moved("protect", failed=False)
+    mon.moved("protect", failed=False)
+    mon.moved("protect", failed=False)
     assert mon.protect_counter == 2
 
-    mon._moved("geomancy", failed=False)
+    mon.moved("geomancy", failed=False)
     assert mon.protect_counter == 0
 
 
@@ -107,13 +107,13 @@ def test_pokemon_as_strs():
     mon._active = True
     assert str(mon) == "charizard (pokemon object) [Active: True, Status: None]"
 
-    mon._set_hp_status("100/100 slp")
+    mon.set_hp_status("100/100 slp")
     assert str(mon) == "charizard (pokemon object) [Active: True, Status: SLP]"
 
-    mon._cure_status()
+    mon.cure_status()
     assert mon.status is None
 
-    mon._cure_status()
+    mon.cure_status()
     assert mon.status is None
 
 
@@ -121,32 +121,32 @@ def test_pokemon_boosts():
     mon = Pokemon(species="blastoise", gen=8)
     assert set(mon.boosts.values()) == {0}
 
-    mon._boost("accuracy", 1)
+    mon.boost("accuracy", 1)
     assert set(mon.boosts.values()) == {0, 1}
     assert mon.boosts["accuracy"] == 1
 
-    mon._boost("accuracy", 2)
+    mon.boost("accuracy", 2)
     assert mon.boosts["accuracy"] == 3
 
-    mon._boost("accuracy", 2)
+    mon.boost("accuracy", 2)
     assert mon.boosts["accuracy"] == 5
 
-    mon._boost("accuracy", 1)
+    mon.boost("accuracy", 1)
     assert mon.boosts["accuracy"] == 6
 
-    mon._boost("accuracy", -1)
+    mon.boost("accuracy", -1)
     assert mon.boosts["accuracy"] == 5
 
-    mon._boost("accuracy", 5)
+    mon.boost("accuracy", 5)
     assert mon.boosts["accuracy"] == 6
 
-    mon._boost("accuracy", -10)
+    mon.boost("accuracy", -10)
     assert mon.boosts["accuracy"] == -4
 
-    mon._boost("accuracy", -10)
+    mon.boost("accuracy", -10)
     assert mon.boosts["accuracy"] == -6
 
-    mon._boost("spd", 2)
-    mon._clear_negative_boosts()
+    mon.boost("spd", 2)
+    mon.clear_negative_boosts()
     assert mon.boosts["accuracy"] == 0
     assert mon.boosts["spd"] == 2
