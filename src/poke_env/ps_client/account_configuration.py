5edba931e3c2e9eff9e40b214f23d71d2f3f7a2d
Cameron Angliss <cameronangliss99@gmail.com>
2023-08-13 20:08:23 -0400

    bringing back NamedTuple usage

--- src/poke_env/ps_client/account_configuration.py
+++ src/poke_env/ps_client/account_configuration.py
@@ -4,10 +4,11 @@ from typing import Counter, NamedTuple, Optional
 
 CONFIGURATION_FROM_PLAYER_COUNTER: Counter[str] = Counter()
 
-AccountConfiguration = namedtuple("AccountConfiguration", ["username", "password"])
-"""Player configuration object. Represented with a tuple with two entries: username and
-password."""
-
+class AccountConfiguration(NamedTuple):
+    """Player configuration object. Represented with a tuple with two entries: username and
+    password."""
+    username: str
+    password: Optional[str]
 
 
 def _create_account_configuration_from_player(player) -> AccountConfiguration:
