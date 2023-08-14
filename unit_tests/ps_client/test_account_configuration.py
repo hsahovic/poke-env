80dc1da25ea8bbfc18b677cd763956819fa48798
Cameron Angliss <cameronangliss99@gmail.com>
2023-08-13 21:01:44 -0400

    fix unit test

--- unit_tests/ps_client/test_account_configuration.py
+++ unit_tests/ps_client/test_account_configuration.py
@@ -1,60 +1,25 @@
-from poke_env.ps_client.account_configuration import (
-    _create_account_configuration_from_player,
-)
+from poke_env.environment import AbstractBattle
+from poke_env.player import BattleOrder, Player
 
 
 def test_account_configuration_auto_naming():
-    class ShortPlayer:
-        pass
+    class ShortPlayer(Player):
+        def choose_move(self, battle: AbstractBattle) -> BattleOrder:
+            return self.choose_default_move()
 
-    class VeryLongPlayerClassName:
-        pass
+    class VeryLongPlayerClassName(Player):
+        def choose_move(self, battle: AbstractBattle) -> BattleOrder:
+            return self.choose_default_move()
 
-    assert (
-        _create_account_configuration_from_player(ShortPlayer()).username
-        == "ShortPlayer 1"
-    )
-    assert (
-        _create_account_configuration_from_player(ShortPlayer()).username
-        == "ShortPlayer 2"
-    )
-    assert (
-        _create_account_configuration_from_player(VeryLongPlayerClassName()).username
-        == "VeryLongPlayerCl 1"
-    )
-    assert (
-        _create_account_configuration_from_player(VeryLongPlayerClassName()).username
-        == "VeryLongPlayerCl 2"
-    )
-    assert (
-        _create_account_configuration_from_player(VeryLongPlayerClassName()).username
-        == "VeryLongPlayerCl 3"
-    )
-    assert (
-        _create_account_configuration_from_player(VeryLongPlayerClassName()).username
-        == "VeryLongPlayerCl 4"
-    )
-    assert (
-        _create_account_configuration_from_player(VeryLongPlayerClassName()).username
-        == "VeryLongPlayerCl 5"
-    )
-    assert (
-        _create_account_configuration_from_player(VeryLongPlayerClassName()).username
-        == "VeryLongPlayerCl 6"
-    )
-    assert (
-        _create_account_configuration_from_player(VeryLongPlayerClassName()).username
-        == "VeryLongPlayerCl 7"
-    )
-    assert (
-        _create_account_configuration_from_player(VeryLongPlayerClassName()).username
-        == "VeryLongPlayerCl 8"
-    )
-    assert (
-        _create_account_configuration_from_player(VeryLongPlayerClassName()).username
-        == "VeryLongPlayerCl 9"
-    )
-    assert (
-        _create_account_configuration_from_player(VeryLongPlayerClassName()).username
-        == "VeryLongPlayerC 10"
-    )
+    assert ShortPlayer().username == "ShortPlayer 1"
+    assert ShortPlayer().username == "ShortPlayer 2"
+    assert VeryLongPlayerClassName().username == "VeryLongPlayerCl 1"
+    assert VeryLongPlayerClassName().username == "VeryLongPlayerCl 2"
+    assert VeryLongPlayerClassName().username == "VeryLongPlayerCl 3"
+    assert VeryLongPlayerClassName().username == "VeryLongPlayerCl 4"
+    assert VeryLongPlayerClassName().username == "VeryLongPlayerCl 5"
+    assert VeryLongPlayerClassName().username == "VeryLongPlayerCl 6"
+    assert VeryLongPlayerClassName().username == "VeryLongPlayerCl 7"
+    assert VeryLongPlayerClassName().username == "VeryLongPlayerCl 8"
+    assert VeryLongPlayerClassName().username == "VeryLongPlayerCl 9"
+    assert VeryLongPlayerClassName().username == "VeryLongPlayerCl 10"
