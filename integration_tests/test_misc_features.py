f627cc20092f16869503ca79f42f6c82f1f29cdf
Haris Sahovic <25432355+hsahovic@users.noreply.github.com>
2023-10-18 23:47:16 +0200

    Fix avatars (#433)

---
+++ integration_tests/test_misc_features.py
@@ -0,0 +1,32 @@
+import pytest
+
+from poke_env import LocalhostServerConfiguration
+from poke_env.player import RandomPlayer, cross_evaluate
+
+
+@pytest.mark.asyncio
+async def test_avatar():
+    players = [
+        RandomPlayer(
+            server_configuration=LocalhostServerConfiguration,
+            max_concurrent_battles=1,
+            log_level=25,
+            avatar="dragontamer",
+        )
+        for _ in range(2)
+    ]
+    await cross_evaluate(players, n_challenges=1)
+
+
+@pytest.mark.asyncio
+async def test_timer():
+    players = [
+        RandomPlayer(
+            server_configuration=LocalhostServerConfiguration,
+            max_concurrent_battles=1,
+            log_level=25,
+            start_timer_on_battle_start=True,
+        )
+        for _ in range(2)
+    ]
+    await cross_evaluate(players, n_challenges=1)
