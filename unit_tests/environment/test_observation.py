8d5b6cba84ace0b879314ebea4ee2c442e9bec9f
Cayman Simpson <cayman@meta.com>
2024-05-06 20:00:45 -0700

    Added more unit tests for observation class

--- unit_tests/environment/test_observation.py
+++ unit_tests/environment/test_observation.py
@@ -14,53 +14,41 @@ def test_observation(example_request):
     mon = battle.active_pokemon
     battle._opponent_team = {mon.species: mon}
 
-    # Starting turn 1
+    # Observations encode each turn encode, and encode specifically the state of the
+    # battle at the beginning of each turn, and the events that happened on that turn
     battle.parse_message(["", "turn", "1"])
-
-    assert 0 in battle.observations
-
-    assert len(battle._current_observation.weather) == 0
-    assert len(battle._current_observation.fields) == 0
-    assert len(battle._current_observation.fields) == 0
-
     battle.parse_message(["", "-weather", "Sandstorm"])
     battle.parse_message(["", "-fieldstart", "move: Grassy Terrain"])
     battle.parse_message(["", "-fieldstart", "move: Magic Room"])
     battle.parse_message(["", "-sidestart", "p2: RandomPlayer 3", "move: Tailwind"])
     battle.parse_message(["", "-sidestart", "p1: EliteFurretAI", "move: Tailwind"])
     battle.parse_message(["", "-sidestart", "p1: EliteFurretAI", "move: Reflect"])
-
-    # Observation events get captured as they happen
-    assert ["", "-weather", "Sandstorm"] in battle._current_observation.events
-
-    # End turn 1 and store Observation of turn 1, going into turn 2
     battle.parse_message(["", "turn", "2"])
+    battle.parse_message(["", "-weather", "RainyDay"])
+    battle.parse_message(["", "turn", "3"])
+    battle.parse_message(["", "-weather", "SunnyDay"])
+    battle.tied()
 
-    assert battle.observations[1].opponent_team[mon.species] == mon
+    # Check all attributes of Observation, as well as if it updates the battle states
+    # correctly
     assert [
         "",
         "-sidestart",
         "p1: EliteFurretAI",
         "move: Reflect",
     ] in battle.observations[1].events
+    assert mon.species == battle.observations[1].active_pokemon.species
+    assert mon.species == battle.observations[1].opponent_active_pokemon.species
+    assert battle.observations[1].opponent_team[mon.species] == mon
+    assert Weather.SANDSTORM not in battle.observations[1].weather
 
-    # End turn and stoere Observation for turn 2
-    battle.parse_message(["", "turn", "3"])
-    battle.parse_message(["", "-weather", "SunnyDay"])
-
-    # Check Observations and all attributes
-    # TODO: check all observation attributes
-    assert 2 in battle.observations
-    assert 3 not in battle.observations
     assert SideCondition.TAILWIND in battle.observations[2].opponent_side_conditions
     assert SideCondition.TAILWIND in battle.observations[2].side_conditions
     assert SideCondition.REFLECT in battle.observations[2].opponent_side_conditions
-    assert Weather.SANDSTORM not in battle.observations[1].weather
     assert Weather.SANDSTORM in battle.observations[2].weather
     assert Field.GRASSY_TERRAIN in battle.observations[2].fields
     assert Field.MAGIC_ROOM in battle.observations[2].fields
     assert mon.species == battle.observations[2].active_pokemon.species
 
-    # Test whether we store the last turn
-    battle.tied()
     assert ["", "-weather", "SunnyDay"] in battle.observations[3].events
+    assert Weather.SANDSTORM not in battle.observations[3].weather
