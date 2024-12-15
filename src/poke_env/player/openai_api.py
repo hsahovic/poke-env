f257d09ff1467a8d1cf1082fcfaecc29cb567a5b
Cameron Angliss <cameronangliss99@gmail.com>
2024-12-15 00:02:16 -0800

    sync with master

--- src/poke_env/player/openai_api.py
+++ src/poke_env/player/openai_api.py
@@ -114,7 +114,6 @@ class OpenAIGymEnv(
         avatar: Optional[int] = None,
         battle_format: str = "gen8randombattle",
         log_level: Optional[int] = None,
-        accept_open_team_sheet: Optional[bool] = False,
         save_replays: Union[bool, str] = False,
         server_configuration: Optional[
             ServerConfiguration
@@ -179,7 +178,6 @@ class OpenAIGymEnv(
             battle_format=battle_format,
             log_level=log_level,
             max_concurrent_battles=1,
-            accept_open_team_sheet=accept_open_team_sheet,
             save_replays=save_replays,
             server_configuration=server_configuration,
             accept_open_team_sheet=accept_open_team_sheet,
