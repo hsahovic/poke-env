11ef5bd91ce2c220d4cd0a50d7457ac095ac223d
Cameron Angliss <cameronangliss99@gmail.com>
2024-12-16 21:20:10 -0800

    overhaul integration tests

--- integration_tests/test_env_player.py
+++ integration_tests/test_env_player.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 from gymnasium.spaces import Box, Space
-from gymnasium.utils.env_checker import check_env
+from pettingzoo.test import parallel_api_test
 
 from poke_env.player import (
     Gen4EnvSinglePlayer,
@@ -15,7 +15,7 @@ from poke_env.player import (
 
 
 class RandomGen4EnvPlayer(Gen4EnvSinglePlayer):
-    def calc_reward(self, last_battle, current_battle) -> float:
+    def calc_reward(self, battle) -> float:
         return 0.0
 
     def describe_embedding(self) -> Space:
@@ -26,7 +26,7 @@ class RandomGen4EnvPlayer(Gen4EnvSinglePlayer):
 
 
 class RandomGen5EnvPlayer(Gen5EnvSinglePlayer):
-    def calc_reward(self, last_battle, current_battle) -> float:
+    def calc_reward(self, battle) -> float:
         return 0.0
 
     def describe_embedding(self) -> Space:
@@ -37,7 +37,7 @@ class RandomGen5EnvPlayer(Gen5EnvSinglePlayer):
 
 
 class RandomGen6EnvPlayer(Gen6EnvSinglePlayer):
-    def calc_reward(self, last_battle, current_battle) -> float:
+    def calc_reward(self, battle) -> float:
         return 0.0
 
     def describe_embedding(self) -> Space:
@@ -48,7 +48,7 @@ class RandomGen6EnvPlayer(Gen6EnvSinglePlayer):
 
 
 class RandomGen7EnvPlayer(Gen7EnvSinglePlayer):
-    def calc_reward(self, last_battle, current_battle) -> float:
+    def calc_reward(self, battle) -> float:
         return 0.0
 
     def describe_embedding(self) -> Space:
@@ -59,7 +59,7 @@ class RandomGen7EnvPlayer(Gen7EnvSinglePlayer):
 
 
 class RandomGen8EnvPlayer(Gen8EnvSinglePlayer):
-    def calc_reward(self, last_battle, current_battle) -> float:
+    def calc_reward(self, battle) -> float:
         return 0.0
 
     def describe_embedding(self) -> Space:
@@ -70,7 +70,7 @@ class RandomGen8EnvPlayer(Gen8EnvSinglePlayer):
 
 
 class RandomGen9EnvPlayer(Gen9EnvSinglePlayer):
-    def calc_reward(self, last_battle, current_battle) -> float:
+    def calc_reward(self, battle) -> float:
         return 0.0
 
     def describe_embedding(self) -> Space:
@@ -93,7 +93,7 @@ def play_function(player, n_battles):
 def test_random_gym_players_gen4():
     random_player = RandomPlayer(battle_format="gen4randombattle", log_level=25)
     env_player = RandomGen4EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=False
+        log_level=25, start_challenging=False
     )
     env_player.start_challenging(3)
     play_function(env_player, 3)
@@ -103,7 +103,7 @@ def test_random_gym_players_gen4():
 def test_random_gym_players_gen5():
     random_player = RandomPlayer(battle_format="gen5randombattle", log_level=25)
     env_player = RandomGen5EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=False
+        log_level=25, start_challenging=False
     )
     env_player.start_challenging(3)
     play_function(env_player, 3)
@@ -113,7 +113,7 @@ def test_random_gym_players_gen5():
 def test_random_gym_players_gen6():
     random_player = RandomPlayer(battle_format="gen6randombattle", log_level=25)
     env_player = RandomGen6EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=False
+        log_level=25, start_challenging=False
     )
     env_player.start_challenging(3)
     play_function(env_player, 3)
@@ -123,7 +123,7 @@ def test_random_gym_players_gen6():
 def test_random_gym_players_gen7():
     random_player = RandomPlayer(battle_format="gen7randombattle", log_level=25)
     env_player = RandomGen7EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=False
+        log_level=25, start_challenging=False
     )
     env_player.start_challenging(3)
     play_function(env_player, 3)
@@ -133,7 +133,7 @@ def test_random_gym_players_gen7():
 def test_random_gym_players_gen8():
     random_player = RandomPlayer(battle_format="gen8randombattle", log_level=25)
     env_player = RandomGen8EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=False
+        log_level=25, start_challenging=False
     )
     env_player.start_challenging(3)
     play_function(env_player, 3)
@@ -143,7 +143,7 @@ def test_random_gym_players_gen8():
 def test_random_gym_players_gen9():
     random_player = RandomPlayer(battle_format="gen9randombattle", log_level=25)
     env_player = RandomGen9EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=False
+        log_level=25, start_challenging=False
     )
     env_player.start_challenging(3)
     play_function(env_player, 3)
@@ -153,7 +153,7 @@ def test_random_gym_players_gen9():
 def test_two_successive_calls_gen8():
     random_player = RandomPlayer(battle_format="gen8randombattle", log_level=25)
     env_player = RandomGen8EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=False
+        log_level=25, start_challenging=False
     )
     env_player.start_challenging(2)
     play_function(env_player, 2)
@@ -165,7 +165,7 @@ def test_two_successive_calls_gen8():
 def test_two_successive_calls_gen9():
     random_player = RandomPlayer(battle_format="gen9randombattle", log_level=25)
     env_player = RandomGen9EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=False
+        log_level=25, start_challenging=False
     )
     env_player.start_challenging(2)
     play_function(env_player, 2)
@@ -174,40 +174,40 @@ def test_two_successive_calls_gen9():
 
 
 @pytest.mark.timeout(60)
-def test_check_envs():
+def test_parallel_api_tests():
     random_player = RandomPlayer(battle_format="gen4randombattle", log_level=25)
     env_player_gen4 = RandomGen4EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=True
+        log_level=25, start_challenging=True
     )
-    check_env(env_player_gen4)
+    parallel_api_test(env_player_gen4)
     env_player_gen4.close()
     random_player = RandomPlayer(battle_format="gen5randombattle", log_level=25)
     env_player_gen5 = RandomGen5EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=True
+        log_level=25, start_challenging=True
     )
-    check_env(env_player_gen5)
+    parallel_api_test(env_player_gen5)
     env_player_gen5.close()
     random_player = RandomPlayer(battle_format="gen6randombattle", log_level=25)
     env_player_gen6 = RandomGen6EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=True
+        log_level=25, start_challenging=True
     )
-    check_env(env_player_gen6)
+    parallel_api_test(env_player_gen6)
     env_player_gen6.close()
     random_player = RandomPlayer(battle_format="gen7randombattle", log_level=25)
     env_player_gen7 = RandomGen7EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=True
+        log_level=25, start_challenging=True
     )
-    check_env(env_player_gen7)
+    parallel_api_test(env_player_gen7)
     env_player_gen7.close()
     random_player = RandomPlayer(battle_format="gen8randombattle", log_level=25)
     env_player_gen8 = RandomGen8EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=True
+        log_level=25, start_challenging=True
     )
-    check_env(env_player_gen8)
+    parallel_api_test(env_player_gen8)
     env_player_gen8.close()
     random_player = RandomPlayer(battle_format="gen9randombattle", log_level=25)
     env_player_gen9 = RandomGen9EnvPlayer(
-        log_level=25, opponent=random_player, start_challenging=True
+        log_level=25, start_challenging=True
     )
-    check_env(env_player_gen9)
+    parallel_api_test(env_player_gen9)
     env_player_gen9.close()
