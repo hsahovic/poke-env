59caa36c20af0805435c08747d824c4875289c36
Cameron Angliss <cameronangliss99@gmail.com>
2024-12-16 19:31:05 -0800

    removing a bunch of stuff that doesn't exist anymore

--- unit_tests/player/test_env_player.py
+++ unit_tests/player/test_env_player.py
@@ -81,7 +81,7 @@ def test_choose_move(queue_put_mock, queue_get_mock):
     )
     battle = Battle("bat1", player.agent1.username, player.agent1.logger, gen=8)
     battle._available_moves = {Move("flamethrower", gen=8)}
-    message = player.agent.choose_move(battle)
+    message = player.agent1.choose_move(battle)
 
     assert isawaitable(message)
 
@@ -91,177 +91,13 @@ def test_choose_move(queue_put_mock, queue_get_mock):
 
     battle._available_moves = {Pokemon(species="charizard", gen=8)}
 
-    message = player.agent.choose_move(battle)
+    message = player.agent1.choose_move(battle)
 
     assert isawaitable(message)
 
     message = asyncio.get_event_loop().run_until_complete(message)
 
     assert message.message == "/choose switch charizard"
-
-
-def test_reward_computing_helper():
-    player = CustomEnvPlayer(
-        acct_config1=acct_config1,
-        acct_config2=acct_config2,
-        server_configuration=server_configuration,
-        start_listening=False,
-        battle_format="gen7randombattles",
-    )
-    battle_1 = Battle("bat1", player.username, player.logger, gen=8)
-    battle_2 = Battle("bat2", player.username, player.logger, gen=8)
-    battle_3 = Battle("bat3", player.username, player.logger, gen=8)
-    battle_4 = Battle("bat4", player.username, player.logger, gen=8)
-
-    assert (
-        player.reward_computing_helper(
-            battle_1,
-            fainted_value=0,
-            hp_value=0,
-            number_of_pokemons=4,
-            starting_value=0,
-            status_value=0,
-            victory_value=1,
-        )
-        == 0
-    )
-
-    battle_1._won = True
-    assert (
-        player.reward_computing_helper(
-            battle_1,
-            fainted_value=0,
-            hp_value=0,
-            number_of_pokemons=4,
-            starting_value=0,
-            status_value=0,
-            victory_value=1,
-        )
-        == 1
-    )
-
-    assert (
-        player.reward_computing_helper(
-            battle_2,
-            fainted_value=0,
-            hp_value=0,
-            number_of_pokemons=4,
-            starting_value=0.5,
-            status_value=0,
-            victory_value=5,
-        )
-        == -0.5
-    )
-
-    battle_2._won = False
-    assert (
-        player.reward_computing_helper(
-            battle_2,
-            fainted_value=0,
-            hp_value=0,
-            number_of_pokemons=4,
-            starting_value=0,
-            status_value=0,
-            victory_value=5,
-        )
-        == -5
-    )
-
-    battle_3._team = {i: Pokemon(species="slaking", gen=8) for i in range(4)}
-    battle_3._opponent_team = {i: Pokemon(species="slowbro", gen=8) for i in range(3)}
-
-    battle_3._team[0].status = Status["FRZ"]
-    battle_3._team[1]._current_hp = 100
-    battle_3._team[1]._max_hp = 200
-    battle_3._opponent_team[0].status = Status["FNT"]
-    battle_3._opponent_team[1].status = Status["FNT"]
-
-    # Opponent: two fainted, one full hp opponent
-    # You: one half hp mon, one frozen mon
-    assert (
-        player.reward_computing_helper(
-            battle_3,
-            fainted_value=2,
-            hp_value=3,
-            number_of_pokemons=4,
-            starting_value=0,
-            status_value=0.25,
-            victory_value=100,
-        )
-        == 2.25
-    )
-
-    battle_3._won = True
-    assert (
-        player.reward_computing_helper(
-            battle_3,
-            fainted_value=2,
-            hp_value=3,
-            number_of_pokemons=4,
-            starting_value=0,
-            status_value=0.25,
-            victory_value=100,
-        )
-        == 100
-    )
-
-    battle_4._team, battle_4._opponent_team = (
-        battle_3._opponent_team,
-        battle_3._team,
-    )
-    assert (
-        player.reward_computing_helper(
-            battle_4,
-            fainted_value=2,
-            hp_value=3,
-            number_of_pokemons=4,
-            starting_value=0,
-            status_value=0.25,
-            victory_value=100,
-        )
-        == -2.25
-    )
-
-
-def test_action_space():
-    player = CustomEnvPlayer(None, start_listening=False)
-    assert player.action_space == Discrete(len(Gen7EnvSinglePlayer._ACTION_SPACE))
-
-    for PlayerClass, (has_megas, has_z_moves, has_dynamax) in zip(
-        [
-            Gen4EnvSinglePlayer,
-            Gen5EnvSinglePlayer,
-            Gen6EnvSinglePlayer,
-            Gen7EnvSinglePlayer,
-            Gen8EnvSinglePlayer,
-        ],
-        [
-            (False, False, False),
-            (False, False, False),
-            (True, False, False),
-            (True, True, False),
-            (True, True, True),
-        ],
-    ):
-
-        class CustomEnvClass(PlayerClass):
-            def embed_battle(self, *args, **kwargs):
-                return []
-
-            def calc_reward(self, last_battle, current_battle):
-                return 0.0
-
-            def describe_embedding(self):
-                return None
-
-            def get_opponent(self):
-                return None
-
-        p = CustomEnvClass(None, start_listening=False, start_challenging=False)
-
-        assert p.action_space == Discrete(
-            4 * sum([1, has_megas, has_z_moves, has_dynamax]) + 6
-        )
 
 
 @patch(
