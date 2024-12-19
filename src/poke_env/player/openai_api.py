7db0c609d7a350305aaaa4e7dc64dfeb21df7d17
Cameron Angliss <cameronangliss99@gmail.com>
2024-12-19 14:19:56 -0800

    account for struggle and recharge

--- src/poke_env/player/openai_api.py
+++ src/poke_env/player/openai_api.py
@@ -363,6 +363,12 @@ class PokeEnv(ParallelEnv[str, ObsType, ActionType]):
             ]
             dynamax_space = [i + 12 for i in move_space if battle.can_dynamax]
             tera_space = [i + 16 for i in move_space if battle.can_tera]
+            if (
+                not move_space
+                and len(battle.available_moves) == 1
+                and battle.available_moves[0].id in ["struggle", "recharge"]
+            ):
+                move_space = [6]
             return (
                 switch_space
                 + move_space
