f8a27f28e986a0afb0c5cbcf6454eed2b149b54e
Cameron Angliss <cameronangliss99@gmail.com>
2024-12-19 06:46:14 -0800

    fix test

--- integration_tests/test_env_player.py
+++ integration_tests/test_env_player.py
@@ -4,20 +4,12 @@ from gymnasium.spaces import Box, Space
 from pettingzoo.test import parallel_api_test
 
 from poke_env.environment import AbstractBattle
-from poke_env.player import (
-    BattleOrder,
-    Gen4EnvSinglePlayer,
-    Gen5EnvSinglePlayer,
-    Gen6EnvSinglePlayer,
-    Gen7EnvSinglePlayer,
-    Gen8EnvSinglePlayer,
-    Gen9EnvSinglePlayer,
-    Player,
-    PokeEnv,
-)
+from poke_env.player import BattleOrder, Player, PokeEnv
 
 
 class TestEnv(PokeEnv):
+    _ACTION_SPACE = list(range(10))
+
     def calc_reward(self, battle) -> float:
         return 0.0
 
@@ -26,7 +18,7 @@ class TestEnv(PokeEnv):
 
     def embed_battle(self, battle):
         return battle
-    
+
     def action_to_move(self, action, battle):
         return action
 
@@ -45,49 +37,63 @@ def play_function(player: PokeEnv[AbstractBattle, BattleOrder], n_battles):
 
 @pytest.mark.timeout(30)
 def test_random_gym_players_gen4():
-    env_player = TestEnv(log_level=25, start_challenging=False)
+    env_player = TestEnv(
+        battle_format="gen4randombattle", log_level=25, start_challenging=False
+    )
     env_player.start_challenging(3)
     play_function(env_player, 3)
 
 
 @pytest.mark.timeout(30)
 def test_random_gym_players_gen5():
-    env_player = TestEnv(log_level=25, start_challenging=False)
+    env_player = TestEnv(
+        battle_format="gen5randombattle", log_level=25, start_challenging=False
+    )
     env_player.start_challenging(3)
     play_function(env_player, 3)
 
 
 @pytest.mark.timeout(30)
 def test_random_gym_players_gen6():
-    env_player = TestEnv(log_level=25, start_challenging=False)
+    env_player = TestEnv(
+        battle_format="gen6randombattle", log_level=25, start_challenging=False
+    )
     env_player.start_challenging(3)
     play_function(env_player, 3)
 
 
 @pytest.mark.timeout(30)
 def test_random_gym_players_gen7():
-    env_player = TestEnv(log_level=25, start_challenging=False)
+    env_player = TestEnv(
+        battle_format="gen7randombattle", log_level=25, start_challenging=False
+    )
     env_player.start_challenging(3)
     play_function(env_player, 3)
 
 
 @pytest.mark.timeout(30)
 def test_random_gym_players_gen8():
-    env_player = TestEnv(log_level=25, start_challenging=False)
+    env_player = TestEnv(
+        battle_format="gen8randombattle", log_level=25, start_challenging=False
+    )
     env_player.start_challenging(3)
     play_function(env_player, 3)
 
 
 @pytest.mark.timeout(30)
 def test_random_gym_players_gen9():
-    env_player = TestEnv(log_level=25, start_challenging=False)
+    env_player = TestEnv(
+        battle_format="gen9randombattle", log_level=25, start_challenging=False
+    )
     env_player.start_challenging(3)
     play_function(env_player, 3)
 
 
 @pytest.mark.timeout(60)
 def test_two_successive_calls_gen8():
-    env_player = TestEnv(log_level=25, start_challenging=False)
+    env_player = TestEnv(
+        battle_format="gen8randombattle", log_level=25, start_challenging=False
+    )
     env_player.start_challenging(2)
     play_function(env_player, 2)
     env_player.start_challenging(2)
@@ -96,7 +102,9 @@ def test_two_successive_calls_gen8():
 
 @pytest.mark.timeout(60)
 def test_two_successive_calls_gen9():
-    env_player = TestEnv(log_level=25, start_challenging=False)
+    env_player = TestEnv(
+        battle_format="gen9randombattle", log_level=25, start_challenging=False
+    )
     env_player.start_challenging(2)
     play_function(env_player, 2)
     env_player.start_challenging(2)
@@ -105,21 +113,33 @@ def test_two_successive_calls_gen9():
 
 @pytest.mark.timeout(60)
 def test_parallel_api_tests():
-    env_player_gen4 = TestEnv(log_level=25, start_challenging=True)
+    env_player_gen4 = TestEnv(
+        battle_format="gen4randombattle", log_level=25, start_challenging=True
+    )
     parallel_api_test(env_player_gen4)
     env_player_gen4.close()
-    env_player_gen5 = TestEnv(log_level=25, start_challenging=True)
+    env_player_gen5 = TestEnv(
+        battle_format="gen5randombattle", log_level=25, start_challenging=True
+    )
     parallel_api_test(env_player_gen5)
     env_player_gen5.close()
-    env_player_gen6 = TestEnv(log_level=25, start_challenging=True)
+    env_player_gen6 = TestEnv(
+        battle_format="gen6randombattle", log_level=25, start_challenging=True
+    )
     parallel_api_test(env_player_gen6)
     env_player_gen6.close()
-    env_player_gen7 = TestEnv(log_level=25, start_challenging=True)
+    env_player_gen7 = TestEnv(
+        battle_format="gen7randombattle", log_level=25, start_challenging=True
+    )
     parallel_api_test(env_player_gen7)
     env_player_gen7.close()
-    env_player_gen8 = TestEnv(log_level=25, start_challenging=True)
+    env_player_gen8 = TestEnv(
+        battle_format="gen8randombattle", log_level=25, start_challenging=True
+    )
     parallel_api_test(env_player_gen8)
     env_player_gen8.close()
-    env_player_gen9 = TestEnv(log_level=25, start_challenging=True)
+    env_player_gen9 = TestEnv(
+        battle_format="gen9randombattle", log_level=25, start_challenging=True
+    )
     parallel_api_test(env_player_gen9)
     env_player_gen9.close()
