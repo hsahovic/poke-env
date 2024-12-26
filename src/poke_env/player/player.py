e2fb717a3500a0d3f606e339cf6a60db947d67e9
Cameron Angliss <cameronangliss99@gmail.com>
2024-12-26 14:33:17 -0500

    log

--- src/poke_env/player/player.py
+++ src/poke_env/player/player.py
@@ -283,7 +283,7 @@ class Player(ABC):
                 if split_message[2]:
                     request = orjson.loads(split_message[2])
                     battle.parse_request(request)
-                    print("request:", split_message[2])
+                    print("request:", request["side"]["pokemon"])
                     print("active:", battle.active_pokemon)
                     print("team:", battle.team)
                     if battle.move_on_next_request:
