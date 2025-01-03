37f8f280c29aeacacef247ae9d364a5517e20730
Cameron Angliss <cameronangliss99@gmail.com>
2025-01-02 23:59:47 -0500

    fix name

--- integration_tests/test_env_player.py
+++ integration_tests/test_env_player.py
@@ -6,7 +6,7 @@ from pettingzoo.test.parallel_test import parallel_api_test
 from poke_env.player import GymnasiumEnv
 
 
-class TestEnv(GymnasiumEnv):
+class CIEnv(GymnasiumEnv):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.observation_spaces = {
@@ -33,7 +33,7 @@ def play_function(env, n_battles):
 
 @pytest.mark.timeout(30)
 def test_random_gymnasium_players_gen4():
-    env_player = TestEnv(
+    env_player = CIEnv(
         battle_format="gen4randombattle", log_level=25, start_challenging=False
     )
     env_player.start_challenging(3)
@@ -42,7 +42,7 @@ def test_random_gymnasium_players_gen4():
 
 @pytest.mark.timeout(30)
 def test_random_gymnasium_players_gen5():
-    env_player = TestEnv(
+    env_player = CIEnv(
         battle_format="gen5randombattle", log_level=25, start_challenging=False
     )
     env_player.start_challenging(3)
@@ -51,7 +51,7 @@ def test_random_gymnasium_players_gen5():
 
 @pytest.mark.timeout(30)
 def test_random_gymnasium_players_gen6():
-    env_player = TestEnv(
+    env_player = CIEnv(
         battle_format="gen6randombattle", log_level=25, start_challenging=False
     )
     env_player.start_challenging(3)
@@ -60,7 +60,7 @@ def test_random_gymnasium_players_gen6():
 
 @pytest.mark.timeout(30)
 def test_random_gymnasium_players_gen7():
-    env_player = TestEnv(
+    env_player = CIEnv(
         battle_format="gen7randombattle", log_level=25, start_challenging=False
     )
     env_player.start_challenging(3)
@@ -69,7 +69,7 @@ def test_random_gymnasium_players_gen7():
 
 @pytest.mark.timeout(30)
 def test_random_gymnasium_players_gen8():
-    env_player = TestEnv(
+    env_player = CIEnv(
         battle_format="gen8randombattle", log_level=25, start_challenging=False
     )
     env_player.start_challenging(3)
@@ -78,7 +78,7 @@ def test_random_gymnasium_players_gen8():
 
 @pytest.mark.timeout(30)
 def test_random_gymnasium_players_gen9():
-    env_player = TestEnv(
+    env_player = CIEnv(
         battle_format="gen9randombattle", log_level=25, start_challenging=False
     )
     env_player.start_challenging(3)
@@ -87,7 +87,7 @@ def test_random_gymnasium_players_gen9():
 
 @pytest.mark.timeout(60)
 def test_two_successive_calls_gen8():
-    env_player = TestEnv(
+    env_player = CIEnv(
         battle_format="gen8randombattle", log_level=25, start_challenging=False
     )
     env_player.start_challenging(2)
@@ -98,7 +98,7 @@ def test_two_successive_calls_gen8():
 
 @pytest.mark.timeout(60)
 def test_two_successive_calls_gen9():
-    env_player = TestEnv(
+    env_player = CIEnv(
         battle_format="gen9randombattle", log_level=25, start_challenging=False
     )
     env_player.start_challenging(2)
@@ -109,32 +109,32 @@ def test_two_successive_calls_gen9():
 
 @pytest.mark.timeout(60)
 def test_check_envs():
-    env_player_gen4 = TestEnv(
+    env_player_gen4 = CIEnv(
         battle_format="gen4randombattle", log_level=25, start_challenging=True
     )
     parallel_api_test(env_player_gen4)
     env_player_gen4.close()
-    env_player_gen5 = TestEnv(
+    env_player_gen5 = CIEnv(
         battle_format="gen5randombattle", log_level=25, start_challenging=True
     )
     parallel_api_test(env_player_gen5)
     env_player_gen5.close()
-    env_player_gen6 = TestEnv(
+    env_player_gen6 = CIEnv(
         battle_format="gen6randombattle", log_level=25, start_challenging=True
     )
     parallel_api_test(env_player_gen6)
     env_player_gen6.close()
-    env_player_gen7 = TestEnv(
+    env_player_gen7 = CIEnv(
         battle_format="gen7randombattle", log_level=25, start_challenging=True
     )
     parallel_api_test(env_player_gen7)
     env_player_gen7.close()
-    env_player_gen8 = TestEnv(
+    env_player_gen8 = CIEnv(
         battle_format="gen8randombattle", log_level=25, start_challenging=True
     )
     parallel_api_test(env_player_gen8)
     env_player_gen8.close()
-    env_player_gen9 = TestEnv(
+    env_player_gen9 = CIEnv(
         battle_format="gen9randombattle", log_level=25, start_challenging=True
     )
     parallel_api_test(env_player_gen9)
