b89cb65a83a5e01f2c8f46f603bb8a167f6da286
Cameron Angliss <cameronangliss99@gmail.com>
2025-01-04 16:34:17 -0500

    clean up

--- integration_tests/test_env.py
+++ integration_tests/test_env.py
@@ -66,13 +66,13 @@ def test_random_gymnasium_players_gen8():
     play_function(env, 3)
 
 
-@pytest.mark.timeout(60)
+@pytest.mark.timeout(30)
 def test_random_gymnasium_player_doubles_gen8():
     env = CIEnv(
         battle_format="gen8randomdoublesbattle", log_level=25, start_challenging=False
     )
-    env.start_challenging(10)
-    play_function(env, 10)
+    env.start_challenging(3)
+    play_function(env, 3)
 
 
 @pytest.mark.timeout(30)
@@ -82,13 +82,13 @@ def test_random_gymnasium_players_gen9():
     play_function(env, 3)
 
 
-@pytest.mark.timeout(60)
+@pytest.mark.timeout(30)
 def test_random_gymnasium_player_doubles_gen9():
     env = CIEnv(
         battle_format="gen9randomdoublesbattle", log_level=25, start_challenging=False
     )
-    env.start_challenging(10)
-    play_function(env, 10)
+    env.start_challenging(3)
+    play_function(env, 3)
 
 
 @pytest.mark.timeout(60)
