b59ecdcdcebaa4080eb2102058df1aebfa0eab12
Cameron Angliss <cameronangliss99@gmail.com>
2025-01-31 18:40:44 -0800

    none maps to "pass"

--- src/poke_env/player/battle_order.py
+++ src/poke_env/player/battle_order.py
@@ -82,9 +82,9 @@ class DoubleBattleOrder(BattleOrder):
                 + self.second_order.message.replace("/choose ", "")
             )
         elif self.first_order:
-            return self.first_order.message + ", default"
+            return self.first_order.message + ", pass"
         elif self.second_order:
-            return self.second_order.message + ", default"
+            return "/choose pass, " + self.second_order.message.replace("/choose ", "")
         else:
             return self.DEFAULT_ORDER
 
@@ -104,10 +104,11 @@ class DoubleBattleOrder(BattleOrder):
             if orders:
                 return orders
         elif first_orders:
-            return [DoubleBattleOrder(first_order=order) for order in first_orders]
+            return [DoubleBattleOrder(order, None) for order in first_orders]
         elif second_orders:
-            return [DoubleBattleOrder(first_order=order) for order in second_orders]
-        return [DefaultBattleOrder()]
+            return [DoubleBattleOrder(None, order) for order in second_orders]
+        else:
+            return [DoubleBattleOrder(None, None)]
 
 
 class ForfeitBattleOrder(BattleOrder):
--- unit_tests/player/test_battle_orders.py
+++ unit_tests/player/test_battle_orders.py
@@ -35,10 +35,8 @@ def test_double_orders():
         DoubleBattleOrder(mon, move).message
         == "/choose switch lugia, move selfdestruct 2"
     )
-    assert DoubleBattleOrder(mon).message == "/choose switch lugia, default"
-    assert (
-        DoubleBattleOrder(None, move).message == "/choose move selfdestruct 2, default"
-    )
+    assert DoubleBattleOrder(mon).message == "/choose switch lugia, pass"
+    assert DoubleBattleOrder(None, move).message == "/choose pass, move selfdestruct 2"
     assert DoubleBattleOrder().message == "/choose default"
 
     orders = [move, mon]
@@ -53,12 +51,12 @@ def test_double_orders():
         "/choose switch lugia, move selfdestruct 2",
     }
     assert first == {
-        "/choose move selfdestruct 2, default",
-        "/choose switch lugia, default",
+        "/choose move selfdestruct 2, pass",
+        "/choose switch lugia, pass",
     }
     assert second == {
-        "/choose move selfdestruct 2, default",
-        "/choose switch lugia, default",
+        "/choose pass, move selfdestruct 2",
+        "/choose pass, switch lugia",
     }
     assert none == {"/choose default"}
 
