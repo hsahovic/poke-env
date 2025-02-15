dc27bb767d3b249ab15e073704368db574314785
Cameron Angliss <cameronangliss99@gmail.com>
2025-02-14 19:42:57 -0800

    shooting in the dark a bit here

--- src/poke_env/ps_client/ps_client.py
+++ src/poke_env/ps_client/ps_client.py
@@ -122,7 +122,7 @@ class PSClient:
 
     async def handle_threaded_coroutines(self, coro: Any):
         task = asyncio.run_coroutine_threadsafe(coro, self.loop)
-        await asyncio.wrap_future(task)
+        await asyncio.wrap_future(task, loop=asyncio.get_running_loop())
         return task.result()
 
     async def accept_challenge(self, username: str, packed_team: Optional[str]):
