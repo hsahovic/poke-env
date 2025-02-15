8fe14a02e574a30b37177b0d5760521b498663ca
Cameron Angliss <cameronangliss99@gmail.com>
2025-02-14 19:58:26 -0800

    try this maybe?

--- src/poke_env/player/player.py
+++ src/poke_env/player/player.py
@@ -756,7 +756,8 @@ class Player(ABC):
         self.logger.info("Event logged in received in send challenge")
 
         if to_wait is not None:
-            await to_wait.wait()
+            future_wait = asyncio.run_coroutine_threadsafe(to_wait.wait(), self.ps_client.loop)
+            await self.ps_client._wrap_future(future_wait)
 
         start_time = perf_counter()
 
