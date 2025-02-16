83734e3658bfd83ee8915439c879afa6d442e46d
Cameron Angliss <cameronangliss99@gmail.com>
2025-02-16 12:54:01 -0800

    fix

--- src/poke_env/player/player.py
+++ src/poke_env/player/player.py
@@ -345,6 +345,7 @@ class Player(ABC):
                     self.trying_again.set()
                     await self._handle_battle_request(battle)
                 elif split_message[2].startswith("[Invalid choice] Can't pass: "):
+                    self.trying_again.set()
                     await self._handle_battle_request(battle, maybe_default_order=True)
                 elif split_message[2].startswith(
                     "[Invalid choice] Can't switch: You can't switch to an active "
