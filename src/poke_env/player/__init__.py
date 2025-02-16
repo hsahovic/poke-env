ff496051b67d2f0d580f5b5015d2b15e201319d3
Cameron Angliss <cameronangliss99@gmail.com>
2025-02-15 19:36:22 -0800

    revert unnecessary diff

--- src/poke_env/player/__init__.py
+++ src/poke_env/player/__init__.py
@@ -2,12 +2,8 @@
 
 from pettingzoo.utils.env import ActionType, ObsType  # type: ignore[import-untyped]
 
-from poke_env.player import env, player, utils
-from poke_env.player.baselines import (
-    MaxBasePowerPlayer,
-    RandomPlayer,
-    SimpleHeuristicsPlayer,
-)
+from poke_env.player import env, player, random_player, utils
+from poke_env.player.baselines import MaxBasePowerPlayer, SimpleHeuristicsPlayer
 from poke_env.player.battle_order import (
     BattleOrder,
     DefaultBattleOrder,
@@ -16,6 +12,7 @@ from poke_env.player.battle_order import (
 )
 from poke_env.player.env import PokeEnv
 from poke_env.player.player import Player
+from poke_env.player.random_player import RandomPlayer
 from poke_env.player.singles_env import SinglesEnv
 from poke_env.player.utils import (
     background_cross_evaluate,
@@ -28,6 +25,7 @@ from poke_env.ps_client import PSClient
 __all__ = [
     "env",
     "player",
+    "random_player",
     "utils",
     "ActionType",
     "ObsType",
