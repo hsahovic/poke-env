015b9999bb22a769fc7b125129fdf112c2148861
Cameron Angliss <cameronangliss99@gmail.com>
2025-02-16 00:21:06 -0800

    bugfix

--- src/poke_env/player/env.py
+++ src/poke_env/player/env.py
@@ -296,8 +296,13 @@ class PokeEnv(ParallelEnv[str, ObsType, ActionType]):
                 time.sleep(self._TIME_BETWEEN_RETRIES)
         if self.battle1 and not self.battle1.finished:
             if self.battle1 == self.agent1.battle:
-                self.agent1.order_queue.put(ForfeitBattleOrder())
-                self.agent2.order_queue.put(DefaultBattleOrder())
+                if self.agent1.waiting and not self.agent2.waiting:
+                    self.agent1.order_queue.put(ForfeitBattleOrder())
+                elif self.agent2.waiting and not self.agent1.waiting:
+                    self.agent2.order_queue.put(ForfeitBattleOrder())
+                else:
+                    self.agent1.order_queue.put(ForfeitBattleOrder())
+                    self.agent2.order_queue.put(DefaultBattleOrder())
                 self.agent1.battle_queue.get()
                 self.agent2.battle_queue.get()
             else:
