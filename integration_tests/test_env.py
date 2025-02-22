cb335bf6f67961f59ed9cafcb0860c94a3152f69
Cameron Angliss <cameronangliss99@gmail.com>
2025-02-22 14:49:29 -0800

    separate tests

--- integration_tests/test_env.py
+++ integration_tests/test_env.py
@@ -43,6 +43,10 @@ def test_env_run():
         env.start_challenging(3)
         play_function(env, 3)
         env.close()
+
+
+@pytest.mark.timeout(60)
+def test_repeated_runs():
     env = SinglesTestEnv(
         battle_format="gen8randombattle",
         log_level=25,
