d73b66e8890f995e24d40a72f483038fac122ef7
Cameron Angliss <cameronangliss99@gmail.com>
2025-02-22 14:00:39 -0800

    up the timeout

--- integration_tests/test_env.py
+++ integration_tests/test_env.py
@@ -31,7 +31,7 @@ def play_function(env, n_battles):
             done = any(terminated.values()) or any(truncated.values())
 
 
-@pytest.mark.timeout(60)
+@pytest.mark.timeout(120)
 def test_env_run():
     for gen in range(4, 10):
         env = SinglesTestEnv(
