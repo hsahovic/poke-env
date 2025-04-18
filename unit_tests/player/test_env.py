b077566b8fe9febb235aace157ad0daeb2a37cb1
Cameron Angliss <cameronangliss99@gmail.com>
2025-04-18 02:53:06 -0700

    simplify

--- unit_tests/player/test_env.py
+++ unit_tests/player/test_env.py
@@ -68,8 +68,7 @@ def test_async_player():
     assert embed_battle(player.battle_queue.get()) == "battle"
 
 
-def run_env_reset_step_close(agent1_waiting: bool, agent2_waiting: bool):
-    assert not (agent1_waiting and agent2_waiting)
+def test_env_reset_step_close():
     # Create a CustomEnv instance.
     env = CustomEnv(
         account_configuration1=account_configuration1,
@@ -134,12 +133,6 @@ def run_env_reset_step_close(agent1_waiting: bool, agent2_waiting: bool):
 
     # --- Part 2: Test close() ---
     env.close()
-
-
-def test_env_reset_step_close():
-    run_env_reset_step_close(False, False)
-    run_env_reset_step_close(True, False)
-    run_env_reset_step_close(False, True)
 
 
 def render(battle):
