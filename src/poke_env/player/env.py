c9491c9a5c5982af800d52fdb74c69b7f04fe27b
Cameron Angliss <cameronangliss99@gmail.com>
2025-04-22 06:45:53 -0700

    type

--- src/poke_env/player/env.py
+++ src/poke_env/player/env.py
@@ -84,7 +84,7 @@ class _EnvPlayer(Player):
     battle_queue: _AsyncQueue[AbstractBattle]
     order_queue: _AsyncQueue[BattleOrder]
 
-    def __init__(self, *args, **kwargs: Any):
+    def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
         self.battle_queue = _AsyncQueue(create_in_poke_loop(asyncio.Queue, 1))
         self.order_queue = _AsyncQueue(create_in_poke_loop(asyncio.Queue, 1))
