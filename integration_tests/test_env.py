a9b23b421cba2f26a8fbed22c0a14adfe8a9053d
Cameron Angliss <50247523+cameronangliss@users.noreply.github.com>
2025-04-22 01:59:38 -0700

    Introduce `SingleAgentWrapper` (#686)
    * initial commit
    
    * black
    
    * unused imports
    
    * fix this
    
    * don't need this
    
    * fix
    
    * put import back
    
    * import
    
    * fix
    
    * fix
    
    * simplify
    
    * name fix
    
    * simplify
    
    * fix
    
    * introduce SinglesEnv
    
    * fix tests
    
    * unused import
    
    * fix integration tests
    
    * fix examples
    
    * simplify
    
    * update docs
    
    * rename files
    
    * format
    
    * polish
    
    * unused import
    
    * condense code
    
    * polish
    
    * add wrapper file
    
    * add seed stuff
    
    * incorporate into integration tests
    
    * isort
    
    * raise time limit
    
    * fix
    
    * raise time limit
    
    * bugfix
    
    * add strictness
    
    * fix test
    
    * format and parameterize strict
    
    * add parameter
    
    * fix
    
    * format
    
    * obey strictness
    
    * invalid causes default
    
    * fix test
    
    * fix test
    
    * bugfix
    
    * bugfix
    
    * experiment
    
    * debug
    
    * debug
    
    * fix bug
    
    * cleanup
    
    * put tests back to normal
    
    * avoid returning DefaultBattleOrder if at all possible during conversions
    
    * add "fake" parameter which will allow conversions to be fabricated if they are invalid, if at all possible
    
    * add fake as parameter to env
    
    * bugfix
    
    * fix test
    
    * bugfix
    
    * fix
    
    * fix forfeit behavior
    
    * fix trying_again
    
    * simplify
    
    * simplify and remove maybe_default_order logic
    
    * bugfix
    
    * bugfix
    
    * bugfix
    
    * simplify and format
    
    * bugfix
    
    * go back to how it used to be
    
    * better name and bugfix
    
    * bugfix
    
    * attempt simplification
    
    * try connecting all attributes to wrapper
    
    * attempting fix
    
    * fix
    
    * format
    
    * fix mypy, debug
    
    * cover zoroark nonsense
    
    * try changing integration test slightly
    
    * try this
    
    * debug
    
    * a better way
    
    * simplify
    
    * fix
    
    * cleanup
    
    * fix test
    
    * remove accidental diff
    
    * try this improvement idea
    
    * cleanup
    
    * simplify somewhat and a bugfix
    
    * revert name change
    
    * remove unneeded line
    
    * add invalid choice handler
    
    * bugfix
    
    * cleanup
    
    * fix order of messages
    
    * important bugfixing
    
    * Revert "fix order of messages"
    
    This reverts commit bb14bbff2f6ef8f4336850e92f5c754ebcdb3fc7.
    
    * default order if error occurs
    
    * add new invalid action catch
    
    * try adding better coordination, bringing back trying_again control
    
    * reverting back somewhat
    
    * add grounding assertions
    
    * make sure battles match
    
    * increase buffer size well beyond what would ever be needed
    
    * simplify for the love of all that's holy
    
    * better handling of wait
    
    * try this
    
    * debug
    
    * ensuring I'm not crazy
    
    * debug
    
    * bugfix
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * try flush
    
    * use event instead
    
    * bugfix
    
    * try this
    
    * try this
    
    * debug
    
    * debug
    
    * try this
    
    * cleanup
    
    * cleanup
    
    * cleanup and generalizing
    
    * try this
    
    * bring back original trying_again stuff
    
    * screwing this down a bit more sturdy
    
    * bugfix
    
    * bugfix
    
    * debug
    
    * bugfix
    
    * cleanup
    
    * more cleanup
    
    * better compartmentalization
    
    * remove these
    
    * bugfix
    
    * more correct
    
    * cleanup
    
    * reducing scope-creep
    
    * debug
    
    * put this back
    
    * beef up testing
    
    * tighter timing
    
    * make test less flaky by giving more time
    
    * super duper safe
    
    * read all resources before taking any action in order to avoid all possible race conditions
    
    * cleanup
    
    * put tests back down to prior levels
    
    * debug
    
    * test
    
    * try this
    
    * add necessary cleanup
    
    * clearer logic
    
    * clean out prints
    
    * clean prints
    
    * add some unit tests for step and reset
    
    * combine to one test
    
    * fix test
    
    * fix
    
    * add another cycle
    
    * fix
    
    * simplify the test, remove unnecessary while loop, and format
    
    * fix
    
    * make test fail if no order exists
    
    * silence part 3 for now
    
    * expand test
    
    * reduce test back
    
    * try adding close
    
    * format
    
    * more robust test
    
    * debug
    
    * fix test
    
    * simplify
    
    * try this
    
    * debug
    
    * fix
    
    * fix
    
    * fix
    
    * debug
    
    * be more tolerant
    
    * fix bug where we don't properly handle both players doing a "try again" action
    
    * clean diff
    
    * cleanup and consistency
    
    * cleanup prints
    
    * tone down tests, but not all the way down to 3
    
    * fix the conditions here as well
    
    * format
    
    * more readable logic
    
    * separate tests
    
    * whoops
    
    * this should match the other time limits

--- integration_tests/test_env.py
+++ integration_tests/test_env.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pytest
 from gymnasium.spaces import Box
+from gymnasium.utils.env_checker import check_env
 from pettingzoo.test.parallel_test import parallel_api_test
 
-from poke_env.player import SinglesEnv
+from poke_env.player import RandomPlayer, SingleAgentWrapper, SinglesEnv
 
 
 class SinglesTestEnv(SinglesEnv):
@@ -45,6 +46,31 @@ def test_env_run():
         env.close()
 
 
+def single_agent_play_function(env: SingleAgentWrapper, n_battles: int):
+    for _ in range(n_battles):
+        done = False
+        env.reset()
+        while not done:
+            action = env.action_space.sample()
+            _, _, terminated, truncated, _ = env.step(action)
+            done = terminated or truncated
+
+
+@pytest.mark.timeout(60)
+def test_single_agent_env_run():
+    for gen in range(4, 10):
+        env = SinglesTestEnv(
+            battle_format=f"gen{gen}randombattle",
+            log_level=25,
+            start_challenging=False,
+            strict=False,
+        )
+        env = SingleAgentWrapper(env, RandomPlayer())
+        env.env.start_challenging(10)
+        single_agent_play_function(env, 10)
+        env.close()
+
+
 @pytest.mark.timeout(60)
 def test_repeated_runs():
     env = SinglesTestEnv(
@@ -81,4 +107,18 @@ def test_env_api():
             strict=False,
         )
         parallel_api_test(env)
+        env.close()
+
+
+@pytest.mark.timeout(60)
+def test_single_agent_env_api():
+    for gen in range(4, 10):
+        env = SinglesTestEnv(
+            battle_format=f"gen{gen}randombattle",
+            log_level=25,
+            start_challenging=True,
+            strict=False,
+        )
+        env = SingleAgentWrapper(env, RandomPlayer())
+        check_env(env)
         env.close()
--- src/poke_env/player/env.py
+++ src/poke_env/player/env.py
@@ -9,6 +9,8 @@ from typing import Any, Awaitable, Dict, Generic, List, Optional, Tuple, TypeVar
 from weakref import WeakKeyDictionary
 
 from gymnasium.spaces import Space
+from gymnasium.utils import seeding
+from numpy.random import Generator
 from pettingzoo.utils.env import ParallelEnv  # type: ignore[import-untyped]
 
 from poke_env.concurrency import POKE_LOOP, create_in_poke_loop
@@ -240,6 +242,7 @@ class PokeEnv(ParallelEnv[str, ObsType, ActionType]):
         self.agent2_to_move = False
         self.fake = fake
         self.strict = strict
+        self._np_random: Optional[Generator] = None
         self._reward_buffer: WeakKeyDictionary[AbstractBattle, float] = (
             WeakKeyDictionary()
         )
@@ -322,7 +325,8 @@ class PokeEnv(ParallelEnv[str, ObsType, ActionType]):
         options: Optional[Dict[str, Any]] = None,
     ) -> Tuple[Dict[str, ObsType], Dict[str, Dict[str, Any]]]:
         self.agents = [self.agent1.username, self.agent2.username]
-        # TODO: use the seed
+        if seed is not None:
+            self._np_random, seed = seeding.np_random(seed)
         if not self.agent1.battle or not self.agent2.battle:
             count = self._INIT_RETRIES
             while not self.agent1.battle or not self.agent2.battle:
