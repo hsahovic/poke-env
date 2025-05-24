ab21ab153c8181d5ccb8143ab076526b75a45cf4
Cameron Angliss <cameronangliss99@gmail.com>
2025-05-24 11:24:30 -0400

    add docs

--- src/poke_env/ps_client/account_configuration.py
+++ src/poke_env/ps_client/account_configuration.py
@@ -18,6 +18,15 @@ class AccountConfiguration(NamedTuple):
 
     @classmethod
     def generate(cls, key: str) -> AccountConfiguration:
+        """
+        Generates an AccountConfiguration instance using a provided key.
+        The same key can be used multiple times to create multiple usernames.
+
+        For example, using key=MyName, the first username will be MyName 1,
+        the second will be MyName 2, etc.
+
+        The key will be truncated to be a maximum of 18 characters long.
+        """
         CONFIGURATION_FROM_PLAYER_COUNTER.update([key])
         username = "%s %d" % (key, CONFIGURATION_FROM_PLAYER_COUNTER[key])
         if len(username) > 18:
@@ -29,6 +38,13 @@ class AccountConfiguration(NamedTuple):
 
     @classmethod
     def randgen(cls, length: int = 10) -> AccountConfiguration:
+        """
+        Generates an AccountConfiguration instance with a randomly-generated username.
+
+        The username will be generated with lowercase letters and numbers.
+
+        :param length: determines the length of the randomly-generated username.
+        """
         char_space = string.ascii_lowercase + string.digits
         username = "".join(random.choices(char_space, k=length))
         return cls(username, None)
