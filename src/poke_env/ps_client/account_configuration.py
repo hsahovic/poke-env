9b769dbc60528c9e6394618e192cdd76bfe02f72
Cameron Angliss <cameronangliss99@gmail.com>
2025-05-24 11:09:06 -0400

    improved organization

--- src/poke_env/ps_client/account_configuration.py
+++ src/poke_env/ps_client/account_configuration.py
@@ -1,5 +1,9 @@
 """This module contains objects related to player configuration."""
 
+from __future__ import annotations
+
+import random
+import string
 from typing import Counter, NamedTuple, Optional
 
 CONFIGURATION_FROM_PLAYER_COUNTER: Counter[str] = Counter()
@@ -11,3 +15,20 @@ class AccountConfiguration(NamedTuple):
 
     username: str
     password: Optional[str]
+
+    @classmethod
+    def generate(cls, key: str) -> AccountConfiguration:
+        CONFIGURATION_FROM_PLAYER_COUNTER.update([key])
+        username = "%s %d" % (key, CONFIGURATION_FROM_PLAYER_COUNTER[key])
+        if len(username) > 18:
+            username = "%s %d" % (
+                key[: 18 - len(username)],
+                CONFIGURATION_FROM_PLAYER_COUNTER[key],
+            )
+        return cls(username, None)
+
+    @classmethod
+    def randgen(cls, length: int = 10) -> AccountConfiguration:
+        char_space = string.ascii_lowercase + string.digits
+        username = "".join(random.choices(char_space, k=length))
+        return cls(username, None)
