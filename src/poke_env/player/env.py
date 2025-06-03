2c3a686019cb3da02f6a7cfe3d8d8e2cc4f1b323
Cameron Angliss <cameronangliss99@gmail.com>
2025-06-03 13:26:36 -0700

    unused imports

--- src/poke_env/player/env.py
+++ src/poke_env/player/env.py
@@ -8,8 +8,6 @@ from abc import abstractmethod
 from typing import Any, Awaitable, Dict, Generic, List, Optional, Tuple, TypeVar, Union
 from weakref import WeakKeyDictionary
 
-import numpy as np
-import numpy.typing as npt
 from gymnasium.spaces import Space
 from gymnasium.utils import seeding
 from numpy.random import Generator
