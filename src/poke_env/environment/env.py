9bae29df9b064bd25c49310cd754fd5f9b341a24
Cameron Angliss <cameronangliss99@gmail.com>
2025-07-04 20:32:23 -0700

    black

--- src/poke_env/environment/env.py
+++ src/poke_env/environment/env.py
@@ -392,8 +392,7 @@ class PokeEnv(ParallelEnv[str, ObsType, ActionType]):
             if self.battle1 and not self.battle1.finished:
                 assert self.battle2 is not None
                 if not (
-                    self.agent1.order_queue.empty()
-                    and self.agent2.order_queue.empty()
+                    self.agent1.order_queue.empty() and self.agent2.order_queue.empty()
                 ):
                     await asyncio.sleep(2)
                     if not (
@@ -414,9 +413,7 @@ class PokeEnv(ParallelEnv[str, ObsType, ActionType]):
                     await self.agent1.order_queue.async_put(ForfeitBattleOrder())
                     if self.agent2_to_move:
                         self.agent2_to_move = False
-                        await self.agent2.order_queue.async_put(
-                            DefaultBattleOrder()
-                        )
+                        await self.agent2.order_queue.async_put(DefaultBattleOrder())
                 else:
                     assert self.agent2_to_move
                     self.agent2_to_move = False
