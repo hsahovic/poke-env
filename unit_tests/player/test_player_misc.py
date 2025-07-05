a85aeabfa7197e87a4eb7dfe349baa34800a7cea
Cameron Angliss <cameronangliss99@gmail.com>
2025-07-04 18:39:18 -0700

    fix test

--- unit_tests/player/test_player_misc.py
+++ unit_tests/player/test_player_misc.py
@@ -112,7 +112,7 @@ def test_choose_random_move_doubles(pseudo_random, example_doubles_request):
 
     pseudo_random.side_effect = lambda: 0.5
     choice = player.choose_random_move(battle)
-    assert choice.message == "/choose move rapidspin 1, move wildcharge dynamax -1"
+    assert choice.message == "/choose move rapidspin 1, switch raichualola"
 
     pseudo_random.side_effect = lambda: 0.999
     choice = player.choose_random_move(battle)
