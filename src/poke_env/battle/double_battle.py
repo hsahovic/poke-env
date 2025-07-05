6a761922f246fcaed6fb3afc8ece931db95444a9
Cameron Angliss <cameronangliss99@gmail.com>
2025-07-04 19:04:42 -0700

    black

--- src/poke_env/battle/double_battle.py
+++ src/poke_env/battle/double_battle.py
@@ -509,9 +509,7 @@ class DoubleBattle(AbstractBattle):
             if any(self.force_switch) and not self.force_switch[i]:
                 orders[i] += [PassBattleOrder()]
                 continue
-            orders[i] += [
-                SingleBattleOrder(mon) for mon in self.available_switches[i]
-            ]
+            orders[i] += [SingleBattleOrder(mon) for mon in self.available_switches[i]]
             if all(self.force_switch) and len(self.available_switches[0]) == 1:
                 orders[i] += [PassBattleOrder()]
                 continue
