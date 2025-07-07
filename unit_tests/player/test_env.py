6910b17df05591c8802c3c63d02014c874e8d36b
Cameron Angliss <cameronangliss99@gmail.com>
2025-07-06 17:27:24 -0700

    recover lost tests

--- unit_tests/player/test_env.py
+++ unit_tests/player/test_env.py
@@ -5,6 +5,7 @@ from unittest.mock import AsyncMock
 
 import numpy as np
 import numpy.typing as npt
+import pytest
 from gymnasium.spaces import Discrete
 
 from poke_env.battle import (
@@ -398,6 +399,8 @@ def test_singles_action_order_conversions():
         )
         check_action_order_roundtrip(p, Player.create_order(active_pokemon), battle)
         battle._available_switches = []
+        with pytest.raises(ValueError):
+            p.action_to_order(np.int64(9), battle)
         if has_megas:
             battle._can_mega_evolve = True
             assert (
@@ -476,6 +479,8 @@ def test_doubles_action_order_conversions():
             p, DoubleBattleOrder(Player.create_order(active_pokemon)), battle
         )
         battle._available_switches = [[], []]
+        with pytest.raises(ValueError):
+            p.action_to_order(np.array([25, 0]), battle)
         if has_megas:
             battle._can_mega_evolve = [True, True]
             assert (
