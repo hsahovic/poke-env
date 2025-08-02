8a593b5942bd35059063b0b61f35036caa40c250
Cameron Angliss <cameronangliss99@gmail.com>
2025-08-01 17:01:43 -0700

    try this

--- src/poke_env/environment/doubles_env.py
+++ src/poke_env/environment/doubles_env.py
@@ -122,7 +122,8 @@ class DoublesEnv(PokeEnv[Dict[str, ObsType], npt.NDArray[np.int64]]):
                     for j in battle.get_possible_showdown_targets(move, active_mon)
                 ]
                 for i, move in enumerate(active_mon.moves.values())
-                if move in active_mon.available_z_moves and battle.can_z_move[pos]
+                if move.id in [m.id for m in active_mon.available_z_moves]
+                and battle.can_z_move[pos]
             ]
             zmove_space = [i for s in zmove_spaces for i in s]
             dynamax_spaces = [
@@ -133,7 +134,8 @@ class DoublesEnv(PokeEnv[Dict[str, ObsType], npt.NDArray[np.int64]]):
                     )
                 ]
                 for i, move in enumerate(active_mon.moves.values())
-                if move in battle.available_moves[pos] and battle.can_dynamax[pos]
+                if move.id in [m.id for m in battle.available_moves[pos]]
+                and battle.can_dynamax[pos]
             ]
             dynamax_space = [i for s in dynamax_spaces for i in s]
             tera_space = [i + 80 for i in move_space if battle.can_tera[pos]]
