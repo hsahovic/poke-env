07bb901f9ec45673cc2c5487fbace8a93f71f023
Cameron Angliss <cameronangliss99@gmail.com>
2025-08-12 15:32:58 -0700

    fix prepare

--- src/poke_env/battle/pokemon.py
+++ src/poke_env/battle/pokemon.py
@@ -336,8 +336,10 @@ class Pokemon:
     def prepare(self, move_id: str, target: Optional[Pokemon]):
         self.moved(move_id, use=False)
 
-        move_id = Move.retrieve_id(move_id)
-        move = self.moves[move_id]
+        id_ = Move.retrieve_id(move_id)
+        if id_ not in self.moves:
+            self._moves[id_] = Move(id_, gen=self._data.gen, raw_id=move_id)
+        move = self.moves[id_]
 
         self._preparing_move = move
         self._preparing_target = target
