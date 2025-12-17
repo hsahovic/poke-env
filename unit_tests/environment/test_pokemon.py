ebf394eb427998c80386a2ed7ec1a07556318112
Cameron Angliss <50247523+cameronangliss@users.noreply.github.com>
2025-12-17 05:21:35 -0600

    Add ability tracking to strict consistency checking test (#812)
    * got strict tests to pass once
    
    * fix CI
    
    * simpler and more correct approach
    
    * simplify
    
    * simplify
    
    * fix test
    
    * fix tests and cleanup
    
    * better name
    
    * fix doc string
    
    * simplify

--- unit_tests/environment/test_pokemon.py
+++ unit_tests/environment/test_pokemon.py
@@ -342,11 +342,8 @@ def test_temporary():
     assert furret.type_2 is None
     assert furret.damage_multiplier(PokemonType.ICE) == 2
 
-    furret.set_temporary_ability("frisk")
+    furret.temporary_ability = "frisk"
     assert furret.ability == "frisk"
-
-    furret.set_temporary_ability(None)
-    assert furret.ability is None
 
     furret.switch_out({})
     assert furret.ability == "adaptability"
