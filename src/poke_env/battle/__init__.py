e9d568b0bd4cd2cc131b1cad9259f6a384c92b35
Cameron Angliss <cameronangliss99@gmail.com>
2026-02-06 20:51:12 -0500

    fix unit test

--- src/poke_env/battle/__init__.py
+++ src/poke_env/battle/__init__.py
@@ -3,7 +3,7 @@ from poke_env.battle.battle import Battle
 from poke_env.battle.double_battle import DoubleBattle
 from poke_env.battle.effect import Effect
 from poke_env.battle.field import Field
-from poke_env.battle.move import SPECIAL_MOVES, Move
+from poke_env.battle.move import SPECIAL_MOVES, Move, MoveSet
 from poke_env.battle.move_category import MoveCategory
 from poke_env.battle.observation import Observation
 from poke_env.battle.observed_pokemon import ObservedPokemon
@@ -23,6 +23,7 @@ __all__ = [
     "Effect",
     "Field",
     "Move",
+    "MoveSet",
     "MoveCategory",
     "Observation",
     "ObservedPokemon",
